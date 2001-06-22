1b340285936392223a16841531590504bddf9fa4
michaelh <michaelh@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2001-06-22 01:08:24 +0000

    Added bootstrap
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@913 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc-build/sdcc-build-bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Simple shell script that knows enough to download the sdcc build scripts
+# and start them running.
+
+MODULE=sdcc-build
+CVSROOT=:pserver:anonymous@cvs.sdcc.sourceforge.net:/cvsroot/sdcc
+BUILDROOT=$HOME/build
+# -s for quiet operation so that this can be run from a cronjob
+MAKEFLAGS=-s
+
+# Remove the old version
+rm -rf $BUILDROOT/$MODULE
+
+# Checkout the latest version
+cd $BUILDROOT
+cvs -d$CVSROOT co $MODULE
+
+# And spawn onto the actual build
+cd $BUILDROOT
+exec make -s
+
