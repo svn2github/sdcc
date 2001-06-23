94c8c3485b6f0a9698a68ea3e134a339eaf0296a
michaelh <michaelh@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2001-06-23 05:20:40 +0000

    Changed over to the list and per arch directories
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@932 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/lib/bootstrap.mk
+++ sdcc-build/lib/bootstrap.mk
@@ -1,10 +1,10 @@
 BOOTSTRAPLOG=$(TOPDIR)/build.log
 BOOTSTRAPSSHMAILSERVER=shell1.sourceforge.net
-BOOTSTRAPLIST=michaelh@juju.net.nz
+BOOTSTRAPLIST=sdcc-devel@lists.sourceforge.net
 BOOTSTRAPSUBJECT=Automated build output
 BUILDDATE=$(shell date +%Y%m%d)
 TARBALLNAME=$(TOPDIR)/build/sdcc-snapshot-$(TARGET)-$(BUILDDATE).tar.gz
-SNAPSHOTDEST=shell1.sourceforge.net:/home/groups/s/sd/sdcc/htdocs/snapshots
+SNAPSHOTDEST=shell1.sourceforge.net:/home/groups/s/sd/sdcc/htdocs/snapshots/$(TARGET)
 
 update-bootstrap:
 	cp -f sdcc-build-bootstrap.sh ..
@@ -26,4 +26,3 @@ send-build-mail:
 
 kill-ssh-agent:
 	ssh-agent -k
-
