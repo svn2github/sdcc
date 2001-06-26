086579d1db2c24c294761de59539094284dd9931
michaelh <michaelh@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2001-06-26 04:57:38 +0000

    Commented and shifted back to the list
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@948 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/lib/bootstrap.mk
+++ sdcc-build/lib/bootstrap.mk
@@ -1,10 +1,17 @@
+# File to log all main make output to
 BOOTSTRAPLOG=$(TOPDIR)/build.log
+# Machine to ssh into to send the build result out via email
 BOOTSTRAPSSHMAILSERVER=shell1.sourceforge.net
-BOOTSTRAPLIST=michaelh@juju.net.nz
-#BOOTSTRAPLIST=sdcc-devel@lists.sourceforge.net
+# Address to send the build output to
+#BOOTSTRAPLIST=michaelh@juju.net.nz
+BOOTSTRAPLIST=sdcc-devel@lists.sourceforge.net
+# Subject line to use in the build output email
 BOOTSTRAPSUBJECT=Automated build output ($(TARGET))
+# Stamp to append to the build name.
 BUILDDATE=$(shell date +%Y%m%d)
+# Name of the tarball for this target
 TARBALLNAME=$(TOPDIR)/build/sdcc-snapshot-$(TARGET)-$(BUILDDATE).tar.gz
+# Location to copy the tarball to
 SNAPSHOTDEST=shell1.sourceforge.net:/home/groups/s/sd/sdcc/htdocs/snapshots/$(TARGET)
 
 update-bootstrap:
