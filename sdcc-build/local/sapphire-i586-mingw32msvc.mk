a000a1b19706390e4e9a66b97e4ef344694bee0b
michaelh <michaelh@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2001-07-03 23:20:18 +0000

    Disabled packihx
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@1017 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/sapphire-i586-mingw32msvc.mk
+++ sdcc-build/local/sapphire-i586-mingw32msvc.mk
@@ -1,4 +1,4 @@
 CROSSCOMPILING = 1
 TOOLSPREFIX = i586-mingw32msvc-
-SDCCCONFIGUREFLAGS = --prefix=/sdcc --disable-device-lib-build --disable-ucsim
+SDCCCONFIGUREFLAGS = --prefix=/sdcc --disable-device-lib-build --disable-ucsim --disable-packihx
 EXEEXTENSION = .exe
