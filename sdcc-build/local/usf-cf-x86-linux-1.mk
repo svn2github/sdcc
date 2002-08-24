63509f8bc364c4c93519c75886339f5b348541e1
michaelh <michaelh@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2002-08-24 21:42:40 +0000

    Turned off win32 cross compilation in the cf
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@2076 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/usf-cf-x86-linux-1.mk
+++ sdcc-build/local/usf-cf-x86-linux-1.mk
@@ -1,4 +1,6 @@
 # Dual processor, but make doesn't detect conflicts properly
 MAKEJOBFLAGS =
 # Can build for Linux x86 and win32 x86
-OTHERTARGETS = i586-mingw32msvc
+# mlh: Aug 24 2002 Disabled for now until the cross compilation tools
+# are re-added.
+#OTHERTARGETS = i586-mingw32msvc
