a7407ce2946e390898c19ce6c823b51dd2b5eab9
bernhardheld <bernhardheld@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2003-02-16 18:39:21 +0000

    * local/usf-cf-x86-linux-1-i586-mingw32msvc.mk: moved cross-tools to ~/local/
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@2276 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/usf-cf-x86-linux-1-i586-mingw32msvc.mk
+++ sdcc-build/local/usf-cf-x86-linux-1-i586-mingw32msvc.mk
@@ -1,4 +1,4 @@
 CROSSCOMPILING = 1
-TOOLSPREFIX = $(HOME)/cross-tools/bin/i586-mingw32msvc-
+TOOLSPREFIX = $(HOME)/local/cross-tools/bin/i586-mingw32msvc-
 SDCCCONFIGUREFLAGS = --prefix=/sdcc --disable-device-lib-build --disable-ucsim
 EXEEXTENSION = .exe
