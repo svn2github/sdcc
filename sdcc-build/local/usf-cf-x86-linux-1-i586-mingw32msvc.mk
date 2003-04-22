ea6e90ebe39fe0ce8ad09436cc298efc380a9987
bernhardheld <bernhardheld@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2003-04-22 21:03:50 +0000

    * local/usf-cf-x86-linux-1-i586-mingw32msvc.mk: adapted to new configure
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@2558 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/usf-cf-x86-linux-1-i586-mingw32msvc.mk
+++ sdcc-build/local/usf-cf-x86-linux-1-i586-mingw32msvc.mk
@@ -2,5 +2,12 @@
 MAKEJOBFLAGS = -j 2
 CROSSCOMPILING = 1
 TOOLSPREFIX = $(HOME)/local/cross-tools/bin/i586-mingw32msvc-
-SDCCCONFIGUREFLAGS = --prefix=/sdcc --disable-device-lib-build --disable-ucsim
+SDCCCONFIGUREFLAGS = \
+--prefix=/sdcc \
+--datadir=/sdcc \
+docdir=/sdcc/doc \
+include_dir_suffix=include \
+lib_dir_suffix=lib \
+sdccconf_h_dir_separator=\\\\ \
+--disable-device-lib-build --disable-ucsim
 EXEEXTENSION = .exe
