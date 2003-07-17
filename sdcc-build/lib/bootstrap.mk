19ef4a731cf2ae84f4440bb309f3db0a11f8ff6e
sdcc-builder <sdcc-builder@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2003-07-17 15:32:51 +0000

    lib/bootstrap.mk: build own recode
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@2746 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/lib/bootstrap.mk
+++ sdcc-build/lib/bootstrap.mk
@@ -93,7 +93,7 @@ generate-tarball:
 ifneq ($(CROSSCOMPILING), 1)
 	-cd $(BUILDDIR)/..; tar czf $(TARBALLNAME) sdcc
 else
-	-find $(BUILDDIR) -name "*.txt" -exec /lib/ld-linux.so.2 --library-path $(HOME)/local/lib/ $(HOME)/local/bin/recode lat1..ibmpc {} \;
+	-find $(BUILDDIR) -name "*.txt" -exec recode lat1..ibmpc {} \;
 	-cd $(BUILDDIR)/..; zip -9r $(TARBALLNAME) sdcc
 endif
 
