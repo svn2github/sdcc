a38c759ad0585eb2cb8bbf8d01a97a069f477b70
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2005-08-22 19:42:25 +0000

    use /usr/ccs/bin/strip instead the default GNU /usr/local/bin/strip, which has problems
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@3871 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/sparc-solaris1.mk
+++ sdcc-build/local/sparc-solaris1.mk
@@ -2,4 +2,6 @@
 # Dual processor
 MAKEJOBFLAGS = -j 2
 TARGETOS = sparc-sun-solaris2.9
-HOSTOS = sparc-sun-solaris2.9 
+HOSTOS = sparc-sun-solaris2.9
+# the deafult GNU strip at /usr/local/bin has problems...
+TARGETSTRIP=/usr/ccs/bin/strip
