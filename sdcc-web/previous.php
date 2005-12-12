934b9619de52d8036e4e78dd26aa696e1130e095
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2005-12-12 08:22:16 +0000

    * previous.php, snap_header.php, footer.php, index.php, links.php, snap.php:   XHTML 1.0 Transitional
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@4010 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-web/previous.php
+++ sdcc-web/previous.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
   <title>SDCC Previous News and Changes</title>
-  <link rel="stylesheet" href="style.css" type="text/css">
+  <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body link="teal" vlink="#483d8b">
 <div align="left">
@@ -37,7 +37,7 @@ known working version of Cygwin here on Sourceforge.</p>
       <p>AVR support is continuing. Sandeep expects a usable build
 sometime before the end of October.</p>
       <p> </p>
-      <hr width="60%">
+      <hr width="60%" />
       <p></p>
       <p><i><b>V2.2.0 is now released. </b></i></p>
       <p><i><b>(Feb 27,2000)</b></i> SDCC
