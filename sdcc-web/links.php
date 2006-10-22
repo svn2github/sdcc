e4668b8ae4f27ffe20a785cd3b79b4e4f9f4c42c
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2006-10-22 14:35:28 +0000

    * index.php, snap_header.php, links.php, previous.php:   added favicon, thanks to Maarten Brock
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@4425 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-web/links.php
+++ sdcc-web/links.php
@@ -4,6 +4,7 @@
   <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
   <title>SDCC links page</title>
   <link type="text/css" href="style.css" rel="stylesheet" />
+  <link rel="shortcut icon" type="image/x-icon" href="/images/sdcc.ico" />
 </head>
 <body link="teal" vlink="#483d8b">
 <div align="left">
--- sdcc-web/previous.php
+++ sdcc-web/previous.php
@@ -4,6 +4,7 @@
   <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
   <title>SDCC Previous News and Changes</title>
   <link rel="stylesheet" href="style.css" type="text/css" />
+  <link rel="shortcut icon" type="image/x-icon" href="/images/sdcc.ico" />
 </head>
 <body link="teal" vlink="#483d8b">
 <div align="left">
