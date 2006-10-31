1fc8f6f8bcda6795569a960d6e51badd773496d6
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2006-10-31 07:57:54 +0000

    * lib/upload.mk: keep only the newest 7 files in changelog_heads
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@4450 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/lib/upload.mk
+++ sdcc-build/lib/upload.mk
@@ -1,24 +1,25 @@
-# Makefile that uploades everything from $(HTDOCSDIR) to $(SNAPSHOTDEST)
+# Makefile that uploades everything from $(HTDOCSDIR) to $(_WEBSNAPSHOTDEST)
 
 # Host and path used for removing old versions
-WEBHOST=shell1.sourceforge.net
-WEBHTDOCSDIR=/home/groups/s/sd/sdcc/htdocs
+_WEBHOST=shell1.sourceforge.net
+_WEBHTDOCSDIR=/home/groups/s/sd/sdcc/htdocs
 
-WEBSNAPSHOTDIR=$(WEBHTDOCSDIR)/$(TARBALLBASE)
-WEBREGTESTDIR=$(WEBHTDOCSDIR)/$(REGTESTBASE)
+_WEBSNAPSHOTDIR=$(WEBHTDOCSDIR)/$(TARBALLBASE)
+_WEBREGTESTDIR=$(WEBHTDOCSDIR)/$(REGTESTBASE)
+_WEBCHANGELOGDIR=$(WEBHTDOCSDIR)/changelog_heads
 
 # Location to copy the tarball to
-SNAPSHOTDEST=$(WEBHOST):$(WEBHTDOCSDIR)
+_WEBSNAPSHOTDEST=$(_WEBHOST):$(_WEBHTDOCSDIR)
 
 .PHONY: do-upload remove-old-versions
 do-upload:
 	cd $(STAGINGBASE); \
-	rsync -r --include='*.exe' -e ssh --size-only $(HTDOCSDIR)/* $(SNAPSHOTDEST) && \
+	rsync -r --include='*.exe' -e ssh --size-only $(HTDOCSDIR)/* $(_WEBSNAPSHOTDEST) && \
 	rm -rf $(HTDOCSDIR)/*
 
 # Removes old versions
 remove-old-versions:
 # over seven days old
-#	ssh $(WEBHOST) 'cd $(WEBSNAPSHOTDIR); find . -mtime +7 -not -type d -exec rm {} \;'
+#	ssh $(_WEBHOST) 'cd $(_WEBSNAPSHOTDIR); find . -mtime +7 -not -type d -exec rm {} \;'
 # more than 7 files in dir
-	ssh $(WEBHOST) 'find $(WEBSNAPSHOTDIR)/* $(WEBREGTESTDIR)/* -type d -exec sh -c "cd {} ; ls -1t | sed 1,7d | xargs rm -f" \;'
+	ssh $(_WEBHOST) 'find $(_WEBSNAPSHOTDIR)/* $(_WEBREGTESTDIR)/* $(_WEBCHANGELOGDIR) -type d -exec sh -c "cd {} ; ls -1t | sed 1,7d | xargs rm -f" \;'
