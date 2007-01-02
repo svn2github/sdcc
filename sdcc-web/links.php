d6c67ee84ba81e82ca6ee81d7cc2fec2f71087bb
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2007-01-02 10:59:14 +0000

    * links.php: removed old broken links, new broken links marked as   (Page Not Found), closed [ 948959 ] Add a link Request: link removed
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@4551 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-web/links.php
+++ sdcc-web/links.php
@@ -20,29 +20,24 @@
       <a href="http://www.webring.org/cgi-bin/webring?ring=80x51;list">
 The 8051 Web Ring</a>
 <img src="images/x51chip.jpg" alt="webring icon" align="middle" height="63" width="81" /><br />
-      <a href="http://www.willem.org/nprome.htm">PcGenerator</a>
+      <a href="http://www.willem.org/">PcGenerator</a>
 EpromProgrammer,Pic24C and more...<br />
       <a href="http://www.needhams.com">Needham Electronics</a>
 - various device programmers, all sizes, all prices<a href="http://www.sensorella.da.ru"><br />
 Sensorella</a>
 - Dual 8051 board, ADCs and <i>lots</i> of I/O.<br />
       <a href="http://www.systronix.com">Systronix</a>
-- Makers
-of the STEP and STEP+ for the DallasTINI board (a must see for embedded
+- Makers of the STEP and STEP+ for the DallasTINI board (a must see for embedded
 ethernet and Dallas 390's)<br />
+      <span style="color: rgb(255, 102, 0); font-style: italic;">(Page Not Found)</span>
       <a href="http://www.kd0yu.com">KD0YU.COM</a>
 - Misc stuff, mirrors SDCC when it's not doing anything else. (Oct,2000
 - Re-designed)<br />
       <a href="http://www.microcontroller.com">Microcontroller.com</a>
 - Micros and Embedded stuff. Not to be confused with
 microcontrollers.com (Arrow Electronics... what a joke!)<br />
-      <span style="font-style: italic; color: rgb(255, 102, 0);">(Page Not Found)</span>
       <a href="http://www.aesmicro.com/8051.htm">AES Micro</a>
 - nice but a bit expensive, may be just what some one needs though.<br />
-      <span style="font-style: italic; color: rgb(255, 102, 0);">(Page Not Found)</span>
-      <a href="http://home.nc.rr.com/rtpmicro/">RTP Micros</a>
-- 2 very nice Phillips '552 boards with all the trimmings. Nice price
-too.<br />
       <a href="mailto:karl@turbobit.com">Karl
 Bongers</a> has a start on a <a href="http://www.turbobit.com/sdcc.html">Summary/FAQ</a>
 about SDCC. (*highly* recommended reading!)<br />
@@ -59,13 +54,7 @@ site yet)<br />
       <a href="http://www.dcd.pl/">Digital Core Design</a>&nbsp;-
 private Intellectual Property (IP) Core provider and System-on-Chip
 (SoC) design house<br />
-      <a href="http://www.jungo.com/">jungo.com</a>
-- Jungo makes USB,
-PCI and ISA driver development tools, including WinDriver and
-KernelDriver that automate&nbsp;and simplify the development of
-device
-drivers for the Windows, Linux, Solaris, and VxWorks
-operating&nbsp;systems.<br />
+      <span style="color: rgb(255, 102, 0); font-style: italic;">(Page Not Found)</span>
       <a href="http://norum.homeunix.net/%7Ecarl/usbp/">USBp
 Serial Device Programmer</a> - USB-based microcontroller
 programming system for Mac OS X, Linux, BSD, and Windows<br />
@@ -78,11 +67,10 @@ Major Microcontroller Architectures<br />
 system over USB port in a standard DIL40 package<br />
       </p>
       <h2>Links - Code Sources</h2>
-      <p><span style="color: rgb(255, 102, 0); font-style: italic;">(Page Not Found)</span> <a href="http://www4.tpg.com.au/users/void/bkauler">Goofee</a>
-- Graphical Object Orientation, very different, very interesting. <b>NOTE</b>:
-Link updated Jan 6,2001<br />
+      <p>
       <a href="http://www.programmersheaven.com/zone5/cat27/index.htm">Programmers
 Heaven</a> - 8051 resources, sample code and compilers<br />
+      <span style="color: rgb(255, 102, 0); font-style: italic;">(Page Not Found)</span>
       <a href="http://www.moonbounce.dk/embedded.htm">Carsten
 Groen,
 OZ9AAR</a> - This looked interesting, a Dallas DS5000 to IDE Hard
@@ -96,28 +84,24 @@ snippets.<br />
 - a portable, open source, mini Real Time Kernel<br />
       </p>
       <h2>Links - Circuit Boards and EDA software</h2>
-      <p> <a href="http://www.geocities.com/pdmtr">PC
+      <p>
+      <a href="http://www.geocities.com/pdmtr">PC
 board HOWTO using Press &amp; Peel Tech Film</a><br />
-      <span style="color: rgb(255, 102, 0); font-style: italic;">
-(Page Not Found)</span> <a href="http://www.teckniks.com">Teckniks</a>
-- makers of the Press-n-Peel transfer film.<br />
       <a href="http://www.cadsoft.de">CadSoft</a> -
 makers of
 Eagle EDA software, download a free (somewhat restricted) version for
 Win32 or Linux.<br />
       </p>
       <h2>Links - IC Data and Searches</h2>
-      <p><span style="color: rgb(255, 102, 0); font-style: italic;">(Page Not Found)</span> <a href="http://www.hitex.com/chipdir/chipdir.htm">The
-Chip Directory</a> - Searchable web space for all kinds of semi's<br />
+      <p>
       <a href="http://www.datasheetlocator.com">DataSheetLocator</a>
 - This is just the most fabulous web site I have been to in a long
 time. Need info? Go here!<br />
-      <span style="color: rgb(255, 102, 0); font-style: italic;">
-(Page Not Found)</span> <a href="http://www.iox.co.za/%7Eepanorama.html">ePanorama</a>
-- interesting site for electronic information.<br />
       </p>
       <h2>Links - Embedded Ethernet</h2>
-      <p> <a href="http://www.egnite.de/ethernut/">Ethernut</a>
+      <p>
+      <span style="color: rgb(255, 102, 0); font-style: italic;">(Page Not Found)</span>
+      <a href="http://www.egnite.de/ethernut/">Ethernut</a>
 - a
 small embedded ethernet add-on for micros. Full internet node support.
 This is an open hardware and software project.<br />
@@ -126,12 +110,13 @@ Cornelius</a>
 has a <a href="http://www.titro.de/ethernet/">web page</a>
 devoted to embedding the RealTek RTL8019AS ethernet controller chip
 into a microcontroller. Lots of small tips on wiring, what to do, and
-what not to do.</p>
+what not to do.<br />
+      </p>
       <h2>Links - LCD Stuff</h2>
-      <p><b>T6963 controlled LCD panels </b>(includes
+      <p>
+      <b>T6963 controlled LCD panels </b>(includes
 the Optrex DM-5000 series and others) </p>
-      <p> A couple of
-pictures of an lcd panel with a familiar phrase.<br />
+      <p> A couple of pictures of an lcd panel with a familiar phrase.<br />
       <a href="lcd/MVC-003X.JPG">LCD picture 1 - full view</a>
 Feb 15,2000 10:54:02 PM (133K jpg) <br />
       <a href="lcd/MVC-006X.JPG">LCD picture 2 - close up</a>
@@ -146,14 +131,16 @@ yet ;-) This is alpha-ware at best. It works in my system. Through the
 winter of 2000-01 I will be updating the code and writing some docs.
 This may force me to write better code if I have to document it. Your
 feedbeed back on the code is welcome. If you have enhanced the code,
-made a better mouse trap... please email me and let me know.</p>
+made a better mouse trap... please email me and let me know.<br />
+      </p>
       <h2>Links - simulators</h2>
+      <p>
       <a href="http://www.dattalo.com/gnupic/gpsim.html">gpsim</a>
--
-gpsim is a full-featured software simulator for Microchip PIC
+- gpsim is a full-featured software simulator for Microchip PIC
 microcontrollers distributed under the GNU General Public License<br />
       <a href="http://mazsola.iit.uni-miskolc.hu/%7Edrdani/embedded/ucsim/">&micro;Csim</a>
-- the old ucsim page
+- the old ucsim page<br />
+      </p>
       <hr />
       <p><i>If you have a link to an embedded
 application, either hardware or software, that you believe would be of
