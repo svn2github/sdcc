bf1028c972635f3e9cb822b9f0f56a9a41fedec5
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2007-03-07 10:02:04 +0000

    * local/cf-x86.mk: cf-x86 is an amd64-unknown-linux2.4 machine
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@4668 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/cf-x86.mk
+++ sdcc-build/local/cf-x86.mk
@@ -1,6 +1,10 @@
-MAKEJOBFLAGS =
-TARGETOS = i386-unknown-linux2.4
-HOSTOS = i386-unknown-linux2.4
+# AMD Sempron with Fedora Core release 6
+MAKEJOBFLAGS = -j 2
+TARGETOS = amd64-unknown-linux2.4
+HOSTOS = amd64-unknown-linux2.4
+
+# Enable the pic16 regression tests
+REGTESTTARGETS += test-pic16
 
 # cross-tools are installed
 OTHERTARGETS = i586-mingw32msvc
@@ -10,4 +14,3 @@ OTHERTARGETS = i586-mingw32msvc
 SDCCCONFIGUREFLAGS = --enable-doc
 # Run the doc Makefile
 OPT_ENABLE_DOC = 1
-
