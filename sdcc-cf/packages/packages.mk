91d416b94158f5aef45056c30059a1751c217d6f
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2007-03-11 19:20:24 +0000

    * packages/packages.mk: defined LOCAL_DIR using $(HOME)/... instead of   ~/... so that --prefix=$(LOCAL_DIR) works * packages/build.nsis/Makefile: removed ABS_LOCAL_DIR
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@4680 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-cf/packages/packages.mk
+++ sdcc-cf/packages/packages.mk
@@ -1,3 +1,3 @@
 HOSTNAME := $(shell if [ $(shell expr $(shell hostname) : '.*\.') != '0' ]; then expr $(shell hostname) : '\([^.]*\).'; else echo $(shell hostname); fi)
-LOCAL_DIR=~/local-$(HOSTNAME)
+LOCAL_DIR=$(HOME)/local-$(HOSTNAME)
 SF_REPOSITORY=http://umn.dl.sourceforge.net
