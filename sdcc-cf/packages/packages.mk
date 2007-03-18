53a6efe15d00bca831243153366a5681fc43933d
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2007-03-18 17:17:00 +0000

    * packages/build.ncurses/*: added * crontab.txt: added entry for mirror-doors host * packages/packages.mk, packages/build.readline/Makefile:   added support for universal binaries on Darwin * packages/build.gpsim/Makefile:   introduced CFLAGS, CXXFLAGS and LDFLAGS configure settings
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@4702 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-cf/packages/packages.mk
+++ sdcc-cf/packages/packages.mk
@@ -1,3 +1,15 @@
 HOSTNAME := $(shell if [ $(shell expr $(shell hostname) : '.*\.') != '0' ]; then expr $(shell hostname) : '\([^.]*\).'; else echo $(shell hostname); fi)
 LOCAL_DIR=$(HOME)/local-$(HOSTNAME)
 SF_REPOSITORY=http://umn.dl.sourceforge.net
+
+OS = $(shell uname)
+
+CFLAGS += -I$(HOME)/local-$(HOSTNAME)/include
+CXXFLAGS += -I$(HOME)/local-$(HOSTNAME)/include
+LDFLAGS += -L$(HOME)/local-$(HOSTNAME)/lib
+
+ifeq ($(OS), Darwin)
+  CFLAGS += -I/sw/include
+  CXXFLAGS += -I/sw/include
+  LDFLAGS += -L/sw/lib
+endif
