50a56225313d0be9bb89ddd7c6e359f988569077
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2007-03-29 12:06:12 +0000

    reverted to version 4701 due to a wrong commit
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@4723 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/mirror-doors.mk
+++ sdcc-build/local/mirror-doors.mk
@@ -2,9 +2,8 @@
 TARGETOS = ppc-apple-macosx
 HOSTOS = ppc-apple-macosx
 
-# Build universal binaries
 # an old readline version is installed on the machine,
 # so I had to install new one (readline-5.2) to $(HOME)/local-$(HOSTNAME)
-TARGETLDFLAGS = "-isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -L$(HOME)/local-$(HOSTNAME)/lib"
-TARGETCXXFLAGS = "-isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386 -I$(HOME)/local-$(HOSTNAME)/include"
-TARGETCFLAGS = "-isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETLDFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
+TARGETCXXFLAGS = "-I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCFLAGS = "-I$(HOME)/local-$(HOSTNAME)/include"
