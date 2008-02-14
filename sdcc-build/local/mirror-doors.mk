135b562e523d33009cc1ba2dcd92cd694f3e0b8d
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2008-02-14 19:29:13 +0000

    * local/mirror-doors.mk: murror-doors is now Mac OS X 10.5 Leopard   with Xcvode 3.0
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@5010 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/mirror-doors.mk
+++ sdcc-build/local/mirror-doors.mk
@@ -1,4 +1,4 @@
-# PPC G4 on MacOS X 10.4
+# PPC G4 on MacOS X 10.5
 # i386/ppc universal binaries
 TARGETOS = ppc-apple-macosx
 HOSTOS = ppc-apple-macosx
@@ -8,6 +8,6 @@ TARGET_PLATFORM = universal-apple-macosx
 
 # an old readline version is installed on the machine,
 # so I had to install new one (readline-5.2) to $(HOME)/local-$(HOSTNAME)
-TARGETLDFLAGS = "-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -L$(HOME)/local-$(HOSTNAME)/lib"
-TARGETCXXFLAGS = "-O2 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -I$(HOME)/local-$(HOSTNAME)/include"
-TARGETCFLAGS = "-O2 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETLDFLAGS = "-Wl,-syslibroot,/Developer/SDKs/MacOSX10.5.sdk -arch i386 -arch ppc -L$(HOME)/local-$(HOSTNAME)/lib"
+TARGETCXXFLAGS = "-O2 -isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386 -arch ppc -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCFLAGS = "-O2 -isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386 -arch ppc -I$(HOME)/local-$(HOSTNAME)/include"
