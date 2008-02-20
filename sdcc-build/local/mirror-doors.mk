1824cbcb4fd298dbb5c4f4e6ee8dd393395e8598
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2008-02-20 19:58:25 +0000

    * local/mirror-doors.mk: mirror-doors is now Mac OS X 10.5 Leopard   with Xcvode 2.5; binaries generated with Xcode 3.0 seems not to be   backward compatible with Mac OS X 10.4 and older
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@5030 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/mirror-doors.mk
+++ sdcc-build/local/mirror-doors.mk
@@ -1,5 +1,7 @@
-# PPC G4 on MacOS X 10.5
+# PPC G4 on MacOS X 10.5 using Xcode 2.5
 # i386/ppc universal binaries
+# NOTE: Binaries generated with Xcode 3.0 seems not to be backward compatible with Mac OS X 10.4 and older
+
 TARGETOS = ppc-apple-macosx
 HOSTOS = ppc-apple-macosx
 
@@ -8,6 +10,6 @@ TARGET_PLATFORM = universal-apple-macosx
 
 # an old readline version is installed on the machine,
 # so I had to install new one (readline-5.2) to $(HOME)/local-$(HOSTNAME)
-TARGETLDFLAGS = "-Wl,-syslibroot,/Developer/SDKs/MacOSX10.5.sdk -arch i386 -arch ppc -L$(HOME)/local-$(HOSTNAME)/lib"
-TARGETCXXFLAGS = "-O2 -isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386 -arch ppc -I$(HOME)/local-$(HOSTNAME)/include"
-TARGETCFLAGS = "-O2 -isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386 -arch ppc -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETLDFLAGS = "-Wl,-syslibroot,/Xcode2.5/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -L$(HOME)/local-$(HOSTNAME)/lib"
+TARGETCXXFLAGS = "-O2 -isysroot /Xcode2.5/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCFLAGS = "-O2 -isysroot /Xcode2.5/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -I$(HOME)/local-$(HOSTNAME)/include"
