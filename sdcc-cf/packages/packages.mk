6fb59c5548c7ccd5cb6abb4136ceed2c13c94453
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2008-09-14 13:22:58 +0000

    * packages/build.popt/*: added * packages/build.gpsim/Makefile: use glibtoolize on Mac OS X * packages/build.ncurses/Makefile: use Xcode2.5 on Mac OS X * packages/packages.mk: remove /sw/* include and lib search path
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@5237 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-cf/packages/packages.mk
+++ sdcc-cf/packages/packages.mk
@@ -7,9 +7,3 @@ OS = $(shell uname)
 CFLAGS += -I$(HOME)/local-$(HOSTNAME)/include
 CXXFLAGS += -I$(HOME)/local-$(HOSTNAME)/include
 LDFLAGS += -L$(HOME)/local-$(HOSTNAME)/lib
-
-ifeq ($(OS), Darwin)
-  CFLAGS += -I/sw/include
-  CXXFLAGS += -I/sw/include
-  LDFLAGS += -L/sw/lib
-endif
