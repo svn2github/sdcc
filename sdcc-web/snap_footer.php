7821286ca8eb98b0b8802e8a0f301b89739d9a2e
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2008-11-14 06:42:09 +0000

    * snap_footer.php: direct access not possible any more
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@5265 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-web/snap_footer.php
+++ sdcc-web/snap_footer.php
@@ -1,3 +1,4 @@
+<!-- direct access not possible any more - borutr 2008-11-14
           <h2><a name="What"></a>Direct Access</h2>
           <p>
 You can also directly access the raw directories where these
@@ -6,6 +7,7 @@ You can also directly access the raw directories where these
 <a href="http://sdcc.sourceforge.net/changelog_heads/">ChangeLog heads</a>
 are stored, rather than using this pretty html version.
           </p>
+-->
 <?php include('./footer.php')?>
         </td>
       </tr>
