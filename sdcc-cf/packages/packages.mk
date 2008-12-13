1b7c5e311ea701787b66dffc78ff3b370a1d6200
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2008-12-13 07:14:06 +0000

    * packages/packages.mk, packages/build.scons/Makefile,   packages/build.nsis/Makefile: upgraded to SCons 1.1.0, NSIS 2.41
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@5281 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-cf/packages/packages.mk
+++ sdcc-cf/packages/packages.mk
@@ -1,6 +1,6 @@
 HOSTNAME := $(shell if [ $(shell expr $(shell hostname) : '.*\.') != '0' ]; then expr $(shell hostname) : '\([^.]*\).'; else echo $(shell hostname); fi)
 LOCAL_DIR=$(HOME)/local-$(HOSTNAME)
-SF_REPOSITORY=http://umn.dl.sourceforge.net
+SF_REPOSITORY=http://voxel.dl.sourceforge.net
 
 OS = $(shell uname)
 
