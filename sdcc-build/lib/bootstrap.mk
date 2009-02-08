6897b22f5595c6a02932b95bb7c37cbf99da1c03
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2009-02-08 11:36:08 +0000

    * lib/bootstrap.mk: add COPYING3.txt to win32 setup package
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@5362 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/lib/bootstrap.mk
+++ sdcc-build/lib/bootstrap.mk
@@ -77,6 +77,7 @@ generate-setup:
 ifeq ($(CROSSCOMPILING), 1)
 	mkdir -p $(dir $(SETUPNAME))
 	cp $(ORIGDIR)/sdcc/COPYING $(BUILDDIR)$(PREFIX)/COPYING.txt; unix2dos $(BUILDDIR)$(PREFIX)/COPYING.txt
+	cp $(ORIGDIR)/sdcc/ar/COPYING3 $(BUILDDIR)$(PREFIX)/COPYING3.txt; unix2dos $(BUILDDIR)$(PREFIX)/COPYING3.txt
 	cp $(ORIGDIR)/sdcc/support/scripts/sdcc.nsi $(BUILDDIR)$(PREFIX)
 	cp $(ORIGDIR)/sdcc/support/scripts/sdcc.ico $(BUILDDIR)$(PREFIX)
 	-cd $(BUILDDIR)$(PREFIX); \
