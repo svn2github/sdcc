a916a94a02768db43aa501888618e1dc69bc8202
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-04-05 12:29:52 +0000

    - use one make processor on Solaris
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@5783 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/dev.mk
+++ sdcc-build/local/dev.mk
@@ -1,6 +1,6 @@
 # Solaris on sparc
 # Dual processor
-MAKEJOBFLAGS = -j 2
+#MAKEJOBFLAGS = -j 2
 TARGETOS = sparc-sun-solaris
 HOSTOS = sparc-sun-solaris
 
--- sdcc-build/local/solaris-x86.mk
+++ sdcc-build/local/solaris-x86.mk
@@ -1,6 +1,6 @@
 # Solaris on sparc
 # Dual processor
-MAKEJOBFLAGS = -j 2
+#MAKEJOBFLAGS = -j 2
 TARGETOS = i386-sun-solaris
 HOSTOS = i386-sun-solaris
 
