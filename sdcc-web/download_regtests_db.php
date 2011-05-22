4430882454f7bdc32c7dc5d2cf6d973e2ca3cd5c
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-05-22 17:23:15 +0000

    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@6549 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc-web/download_regtests_db.php
@@ -0,0 +1,114 @@
+<?php
+/*-------------------------------------------------------------------------
+   download_regtest_db.php - download regression tests database
+                             in CSV format
+
+   Copyright (C) 2011, Borut Razem <borut.razem AT gmail.com>
+
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2.1, or (at your option) any
+   later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License 
+   along with this library; see the file COPYING. If not, write to the
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+
+   As a special exception, if you link this library with other files,
+   some of which are compiled with SDCC, to produce an executable,
+   this library does not by itself cause the resulting executable to
+   be covered by the GNU General Public License. This exception does
+   not however invalidate any other reasons why the executable file
+   might be covered by the GNU General Public License.
+-------------------------------------------------------------------------*/
+
+class Out {
+  protected $fileExt;
+  protected $view;
+
+  function gen_http_header()
+  {
+    header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+    header('Content-Type: application/ms-excel; charset=utf-8');
+    header('Content-Disposition: attachment; filename="' . $this->view . $this->fileExt . '"');
+  }
+
+  function prolog()
+  {
+  }
+
+  function epilog()
+  {
+  }
+
+  function gen_head ($row)
+  {
+  }
+
+  function gen_row ($row)
+  {
+  }
+}
+
+class Csv extends Out {
+  protected $outstream;
+
+  function __construct($view, $encoding = "windows-1250//IGNORE")
+  {
+    $this->view = $view;
+    $this->encoding = $encoding;
+    $this->fileExt = '.csv';
+    $this->outstream = fopen('php://output', 'w');
+  }
+
+  function gen_head($row)
+  {
+    $this->gen_row($row);
+  }
+
+  private function recode_elem($elem)
+  {
+    return iconv("utf-8", $this->encoding, $elem);
+  }
+
+  function gen_row ($row)
+  {
+    $row1 = array_map(array($this, "recode_elem"), $row);
+    fputcsv($this->outstream, $row1, ';');
+  }
+}
+
+$out = new Csv('regression_test_results');
+
+$out->gen_http_header();
+
+$db = $regTestDb = new mysqli('mysql-s', 's599ro', 'riaPhiUp', 's599_regtests');
+
+$query = 'SELECT * FROM regtest_results';
+$result = $db->query($query);
+if ($result) {
+  $out->prolog($view);
+
+  $out->gen_head(array('platform', 'target', 'build_number', 'date', 'regtest_name', 'failures', 'tests', 'cases', 'bytes', 'ticks', 'time_stamp'));
+
+  while ($row = $result->fetch_assoc()) {
+    $out->gen_row($row);
+  }
+
+  $out->epilog();
+  $result->free();
+}
+else {
+  echo("DB Error, could not query the database quey = " . $query . "<br />\n");
+  echo('MySQL Error: ' . mysql_error() . "<br />\n");
+}
+
+$db->close();
+?>
