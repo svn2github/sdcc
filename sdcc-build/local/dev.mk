915b2f0b55c29194bb2833b56989b61e1cb3c040
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-07-21 17:47:55 +0000

    * local/dev.mk, local/solaris-x86.mk:   added TARGETCFLAGS and TARGETLSFLAGS * local/amd64-linux1.mk, local/openpower-linux1.mk, local/ppc-osx2.mk,   local/ppc-osx3.mk, local/sparc-solaris1.mk,   local/usf-cf-alpha-linux-1.mk, local/x86-linux2.mk,   local/x86-linux2-i586-mingw32msvc.mk: moved to local/old
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@6665 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/dev.mk
+++ sdcc-build/local/dev.mk
@@ -5,3 +5,5 @@ TARGETOS = sparc-sun-solaris
 HOSTOS = sparc-sun-solaris
 
 TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETLSFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
--- sdcc-build/local/solaris-x86.mk
+++ sdcc-build/local/solaris-x86.mk
@@ -1,7 +1,9 @@
-# Solaris on sparc
+# Solaris on i386
 # Dual processor
 #MAKEJOBFLAGS = -j 2
 TARGETOS = i386-sun-solaris
 HOSTOS = i386-sun-solaris
 
 TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETLSFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
