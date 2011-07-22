7cd274f741106ad495049d6524926f89e518905c
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-07-22 18:10:44 +0000

    - corrected typo
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@6671 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/solaris-x86.mk
+++ sdcc-build/local/solaris-x86.mk
@@ -6,4 +6,4 @@ HOSTOS = i386-sun-solaris
 
 TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
 TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
-TARGETLSFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
+TARGETLDFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
