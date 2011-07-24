71a8aa4e521000e5eae074dfaf01886d59421b79
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-07-24 09:53:39 +0000

    * support/regression/tests/compare_2_n.c: added test for unsigned long type
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@6678 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/compare_2_n.c
+++ sdcc/support/regression/tests/compare_2_n.c
@@ -1,7 +1,7 @@
 /** Compare with 2^n greater then 255
     test for patch #2702889 - Summary of all uncommitted changes I applied on "my" SDCC
  
-    right: 0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000
+    int_right: 0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000
 */
 
 #include <testfwk.h>
@@ -9,7 +9,7 @@
 int
 lwr (unsigned left)
 {
-  if (left < {right})
+  if (left < {int_right})
     return 1;
   else
     return 0;
@@ -18,7 +18,20 @@ lwr (unsigned left)
 int
 gtr (unsigned left)
 {
-  if (left > {right})
+  return (left > {int_right}) ? 1 : 0;
+}
+
+#define LONG_RIGHT 0x80000000
+int
+long_lwr (unsigned long left)
+{
+  return (left < LONG_RIGHT) ? 1 : 0;
+}
+
+int
+long long_gtr (unsigned long left)
+{
+  if (left > LONG_RIGHT)
     return 1;
   else
     return 0;
@@ -27,10 +40,17 @@ gtr (unsigned left)
 void
 test_lwr_gtr (void)
 {
-  ASSERT (lwr ({right} - 1));
-  ASSERT (!lwr ({right}));
-  ASSERT (!lwr ({right} + 1));
-  ASSERT (gtr ({right} + 1));
-  ASSERT (!gtr ({right}));
-  ASSERT (!gtr ({right} - 1));
+  ASSERT (lwr ({int_right} - 1));
+  ASSERT (!lwr ({int_right}));
+  ASSERT (!lwr ({int_right} + 1));
+  ASSERT (gtr ({int_right} + 1));
+  ASSERT (!gtr ({int_right}));
+  ASSERT (!gtr ({int_right} - 1));
+
+  ASSERT (long_lwr (LONG_RIGHT - 1));
+  ASSERT (!long_lwr (LONG_RIGHT));
+  ASSERT (!long_lwr (LONG_RIGHT + 1));
+  ASSERT (long_gtr (LONG_RIGHT + 1));
+  ASSERT (!long_gtr (LONG_RIGHT));
+  ASSERT (!long_gtr (LONG_RIGHT - 1));
 }
