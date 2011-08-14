75886d3f4ac2ecd884dee41f3771414f8fdc9d26
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-08-14 16:49:54 +0000

    * local/cf-x86.mk: disable x86 build
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@6739 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/cf-x86.mk
+++ sdcc-build/local/cf-x86.mk
@@ -13,7 +13,8 @@ TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
 TARGETLDFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
 
 # cross-tools are installed
-OTHERTARGETS = i586-mingw32msvc i386-unknown-linux2.5
+####OTHERTARGETS = i586-mingw32msvc i386-unknown-linux2.5
+OTHERTARGETS = i586-mingw32msvc
 
 # for backward compatibility
 # FC6 uses --hash-style=gnu by default
