2c2a57f7d3b1fca38998757c7db49b43c4c6aec7
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-08-31 20:44:34 +0000

    * local/cf-x86-x86_64-w64-mingw32.mk, local/cf-x86.mk:   added Windows 64bit mingw_w64 cross-compilation * local/cf-x86-i386-unknown-linux2.5.mk,   local/cf-x86-i586-mingw32msvc.mk: moved to local/old
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@6790 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc-build/local/cf-x86-x86_64-w64-mingw32.mk
@@ -0,0 +1,29 @@
+# Generate 64 bit x86_64 mingw_w64 executables on
+# AMD Sempron with Fedora Core release 6
+
+# Spawn 2 make proceses
+MAKEJOBFLAGS = -j 2
+
+CROSSCOMPILING = 1
+
+TOOLSPREFIX = $(HOME)/local-$(HOSTNAME)/cross-tools-w64/bin/x86_64-w64-mingw32-
+
+PREFIX = /sdcc
+
+CC_FOR_BUILD = gcc
+
+SDCCCONFIGUREFLAGS = \
+--prefix=$(PREFIX) \
+--datarootdir=/sdcc \
+docdir=$${datarootdir}/doc \
+include_dir_suffix=include \
+non_free_include_dir_suffix=non-free/include \
+lib_dir_suffix=lib \
+non_free_lib_dir_suffix=non-free/lib \
+sdccconf_h_dir_separator=\\\\ \
+--disable-device-lib
+EXEEXTENSION = .exe
+
+TARGETCXXFLAGS =
+TARGETCFLAGS =
+TARGETLDFLAGS =
--- sdcc-build/local/cf-x86.mk
+++ sdcc-build/local/cf-x86.mk
@@ -13,7 +13,7 @@ TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
 TARGETLDFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
 
 # cross-tools are installed
-####OTHERTARGETS = i586-mingw32msvc i386-unknown-linux2.5
+OTHERTARGETS = x86_64-w64-mingw32
 
 # for backward compatibility
 # FC6 uses --hash-style=gnu by default
