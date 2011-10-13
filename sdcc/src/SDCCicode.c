906ef7604e33d70f30e9c695715cfd8c7b89edcf
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-10-13 17:41:27 +0000

    * src/SDCCicode.c:   disable -O2 optimization for i386 Linux and mingw platforms   (solaris i386 is OK);   see patch #3285611: Workaround gcc 4.6.0 bug with sdcc 3.0.0 * src/SDCCval.c:   Microsoft C compiler doesn't support hexadecimal floating-point   literals
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@6963 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/SDCCicode.c
+++ sdcc/src/SDCCicode.c
@@ -1128,10 +1128,10 @@ getBuiltinParms (iCode * fic, int *pcount, operand ** parms)
   return ic;
 }
 
-/* This seems to be a GCC 4.6.[01] bug
+/* This seems to be a GCC 4.6.[01] bug on i386 Linux and mingw platforms
  * see http://sourceforge.net/tracker/?func=detail&aid=3285611&group_id=599&atid=300599
  */
-#if defined(__i386__) && defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ == 6 && (__GNUC_PATCHLEVEL__ == 0 || __GNUC_PATCHLEVEL__ == 1)
+#if (defined(__linux__) || defined(__MINGW32__)) && defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ == 6 && (__GNUC_PATCHLEVEL__ == 0 || __GNUC_PATCHLEVEL__ == 1)
 #pragma GCC push_options
 #pragma GCC optimize ("O0")
 #endif
@@ -1390,10 +1390,10 @@ operandOperation (operand * left, operand * right, int op, sym_link * type)
 
   return retval;
 }
-/* This seems to be a GCC 4.6.[01] bug
+/* This seems to be a GCC 4.6.[01] bug on i386 Linux and mingw platforms
  * see http://sourceforge.net/tracker/?func=detail&aid=3285611&group_id=599&atid=300599
  */
-#if defined(__linux__) && defined(__i386__) && defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ == 6 && (__GNUC_PATCHLEVEL__ == 0 || __GNUC_PATCHLEVEL__ == 1)
+#if (defined(__linux__) || defined(__MINGW32__)) && defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ == 6 && (__GNUC_PATCHLEVEL__ == 0 || __GNUC_PATCHLEVEL__ == 1)
 #pragma GCC pop_options
 #endif
 
