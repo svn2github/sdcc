b40723df8982676aab3eb331d4319125151400f4
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-01-16 20:21:14 +0000

    * local/freebsd-i386.mk: disabled -j 2 since src/z80/ralloc2.cc   compilation mysteriously fails if enabled;   added TARGETCPPFLAGS so that the cpp can finde stx and boost   include files during configure
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@7236 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/freebsd-i386.mk
+++ sdcc-build/local/freebsd-i386.mk
@@ -2,7 +2,9 @@
 # Dual processor
 
 # Spawn 2 make proceses
-MAKEJOBFLAGS = -j 2
+## disabled 2012-01-16 by borutr since src/z80/ralloc2.cc compilation
+## mysteriously fails if enabled
+##MAKEJOBFLAGS = -j 2
 
 TARGETOS = i386-unknown-freebsd
 HOSTOS = i386-unknown-freebsd
@@ -10,4 +12,5 @@ HOSTOS = i386-unknown-freebsd
 # local includes and libraries
 TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
 TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCPPFLAGS = "-I$(HOME)/local-$(HOSTNAME)/include"
 TARGETLDFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
