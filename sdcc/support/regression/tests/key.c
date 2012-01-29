2bbe1c529f90465c63271223a98fb319df197031
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-01-29 10:53:55 +0000

    * support/regression/tests/key.c: fixed for Mac OS X
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@7267 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/key.c
+++ sdcc/support/regression/tests/key.c
@@ -3,7 +3,8 @@
 
 #include <testfwk.h>
 
-int _asm, _endasm, at, bit, code, critical, data, far,
+int _asm;
+int _endasm, at, bit, code, critical, data, far,
 eeprom, fixed16x16, flash, idata, interrupt, nonbanked, banked, near,
 pdata, reentrant, shadowregs, wparam, sfr, sfr16, sfr32, sbit, sram,
 using, _naked, xdata, _overlay;
