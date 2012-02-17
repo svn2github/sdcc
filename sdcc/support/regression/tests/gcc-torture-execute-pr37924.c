e5cb92380c92713452e628db4862c6c3759a664c
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-02-17 07:36:14 +0000

    Fix gcc compilation failure.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@7327 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-pr37924.c
+++ sdcc/support/regression/tests/gcc-torture-execute-pr37924.c
@@ -16,14 +16,14 @@ unsigned char b;
 
 #if !(defined (__GNUC__) && defined (__GNUC_MINOR__) && (__GNUC__ < 5 && __GNUC_MINOR < 4))
 int
-test1 (void)
+ftest1 (void)
 {
   int c = -1;
   return ((unsigned int) (a ^ c)) >> 9;
 }
 
 int
-test2 (void)
+ftest2 (void)
 {
   int c = -1;
   return ((unsigned int) (b ^ c)) >> 9;
@@ -35,28 +35,28 @@ testTortureExecute (void)
 {
 #if !(defined (__GNUC__) && defined (__GNUC_MINOR__) && (__GNUC__ < 5 && __GNUC_MINOR < 4))
   a = 0;
-  if (test1 () != (-1U >> 9))
+  if (ftest1 () != (-1U >> 9))
     ASSERT (0);
   a = 0x40;
-  if (test1 () != (-1U >> 9))
+  if (ftest1 () != (-1U >> 9))
     ASSERT (0);
   a = 0x80;
-  if (test1 () != (a < 0) ? 0 : (-1U >> 9))
+  if (ftest1 () != (a < 0) ? 0 : (-1U >> 9))
     ASSERT (0);
   a = 0xff;
-  if (test1 () != (a < 0) ? 0 : (-1U >> 9))
+  if (ftest1 () != (a < 0) ? 0 : (-1U >> 9))
     ASSERT (0);
   b = 0;
-  if (test2 () != (-1U >> 9))
+  if (ftest2 () != (-1U >> 9))
     ASSERT (0);
   b = 0x40;
-  if (test2 () != (-1U >> 9))
+  if (ftest2 () != (-1U >> 9))
     ASSERT (0);
   b = 0x80;
-  if (test2 () != (-1U >> 9))
+  if (ftest2 () != (-1U >> 9))
     ASSERT (0);
   b = 0xff;
-  if (test2 () != (-1U >> 9))
+  if (ftest2 () != (-1U >> 9))
     ASSERT (0);
   return;
 #endif
