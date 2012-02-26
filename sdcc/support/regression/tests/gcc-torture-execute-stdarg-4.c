6f1d2833a4b50140608621f86b2956a52f46f03b
epetrich <epetrich@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-02-26 02:01:20 +0000

    * support/regression/tests/gcc-torture-execute-stdarg-4.c:   reenabled for ds390
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@7358 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-stdarg-4.c
+++ sdcc/support/regression/tests/gcc-torture-execute-stdarg-4.c
@@ -12,7 +12,6 @@
 
 #include <stdarg.h>
 
-#ifndef __SDCC_ds390
 long x, y;
 
 inline void
@@ -121,12 +120,10 @@ f4 (int i, ...)
   va_end (ap);
 }
 
-#endif
 
 void
 testTortureExecute (void)
 {
-#ifndef __SDCC_ds390
   f1 (3, 16.0, 128L, 32.0);
   if (x != 176L)
     ASSERT (0);
@@ -153,6 +150,5 @@ testTortureExecute (void)
     ASSERT (0);
 
   return;
-#endif
 }
 
