9e5c741c98002852ceea7ef6b5a73f4d813441b2
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-02-26 21:54:49 +0000

    * support/regression/ports/ds390/spec.mk:   simulation timeout icreased to 40s due to   gcc-torture-execute-20090113-1 test failure on   x86_64-w64-mingw32 and sparc64-unknown-netbsd platforms * doc/README.txt: added gbz80 to supported targets,   explanation of sdcc run time libraries licensing
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@7368 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/ports/ds390/spec.mk
+++ sdcc/support/regression/ports/ds390/spec.mk
@@ -4,6 +4,9 @@
 
 CC_FOR_BUILD = $(CC)
 
+# simulation timeout in seconds
+SIM_TIMEOUT = 40
+
 # path to uCsim
 ifdef SDCC_BIN_PATH
   S51 = $(SDCC_BIN_PATH)/s51$(EXEEXT)
@@ -59,7 +62,7 @@ $(PORT_CASES_DIR)/fwk.lib: $(srcdir)/fwk/lib/fwk.lib
 # run simulator with 25 seconds timeout
 %.out: %$(BINEXT) $(CASES_DIR)/timeout
 	mkdir -p $(dir $@)
-	-$(CASES_DIR)/timeout 25 $(EMU) -tds390 -S in=$(DEV_NULL),out=$@ $< < $(PORTS_DIR)/$(PORT)/uCsim.cmd > $(@:.out=.sim) \
+	-$(CASES_DIR)/timeout $(SIM_TIMEOUT) $(EMU) -tds390 -S in=$(DEV_NULL),out=$@ $< < $(PORTS_DIR)/$(PORT)/uCsim.cmd > $(@:.out=.sim) \
 	  || echo -e --- FAIL: \"timeout, simulation killed\" in $(<:$(BINEXT)=.c)"\n"--- Summary: 1/1/1: timeout >> $@
 	python $(srcdir)/get_ticks.py < $(@:.out=.sim) >> $@
 	-grep -n FAIL $@ /dev/null || true
