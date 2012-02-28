77c66ec551a6adef581f6c1ce6c441782e18a025
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-02-28 10:05:55 +0000

    * links.php: applied requests from Wbdocs tracker:   #3486131: Broken links   #3407481: Links - General,   #2986975: New Link for SDCC   #2298395: KTechLab   #1807915: KiCAD link
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@7372 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-web/links.php
+++ sdcc-web/links.php
@@ -15,6 +15,7 @@
       <td valign="top" width="15%"><?php echo(file_get_contents('./left_menu.inc'));?><br />
       </td>
       <td width="85%">
+
       <h2>Links - General</h2>
       <p>
       <a href="http://www.webring.org/cgi-bin/webring?ring=80x51;list">
@@ -22,43 +23,43 @@ The 8051 Web Ring</a>
 <img src="images/x51chip.jpg" alt="webring icon" align="middle" height="63" width="81" /><br />
       <a href="http://www.willem.org/">PcGenerator</a>
 EpromProgrammer,Pic24C and more...<br />
-      <a href="http://www.needhams.com">Needham Electronics (dead link?)</a>
-- various device programmers, all sizes, all prices<br />
+      <!-- <a href="http://www.needhams.com">Needham Electronics (dead link?)</a>
+- various device programmers, all sizes, all prices<br /> -->
       <a href="http://www.systronix.com">Systronix</a>
 - Makers of the STEP and STEP+ for the DallasTINI board (a must see for embedded
 ethernet and Dallas 390's)<br />
       <a href="http://www.microcontroller.com">Microcontroller.com</a>
 - Micros and Embedded stuff. Not to be confused with
 microcontrollers.com (Arrow Electronics... what a joke!)<br />
-      <a href="http://www.aesmicro.com/8051.htm">AES Micro (dead link?)</a>
-- nice but a bit expensive, may be just what some one needs though.<br />
-      <a href="mailto:karl@turbobit.com">Karl
+      <!-- <a href="http://www.aesmicro.com/8051.htm">AES Micro (dead link?)</a>
+- nice but a bit expensive, may be just what some one needs though.<br /> -->
+      <!--<a href="mailto:karl@turbobit.com">Karl
 Bongers</a> has a start on a <a href="http://www.turbobit.com/sdcc.html">Summary/FAQ (unrelated link?)</a>
-about SDCC. (*highly* recommended reading!)<br />
+about SDCC. (*highly* recommended reading!)<br /> -->
       <a href="http://www.rabbitsemiconductor.com">Rabbit
 Semiconductor</a> - Z-World based boards, very small and lots of
 I/O. Kits available.<br />
-      <a href="http://www.flexgate.de">FlexGate (unrelated link?)</a>
+      <!-- <a href="http://www.flexgate.de">FlexGate (unrelated link?)</a>
 - '552 based boards with onboard ethernet. (German, not much on the web
-site yet)<br />
-      <a href="http://mikrokontrolery.of.pl/">mikrokontrolery.of.pl</a>
+site yet)<br /> -->
+      <a href="http://mikrokontrolery.net/">mikrokontrolery.of.pl</a>
 - wszystko o mikrokontrolerach jednoukładowych 8051 AVR PIC<br />
-      <a href="http://www.bhargavaz.net/main.html">www.bhargavaz.net (unrelated link?)</a>
--<br />
+      <!-- <a href="http://www.bhargavaz.net/main.html">www.bhargavaz.net (unrelated link?)</a>
+-<br /> -->
       <a href="http://www.dcd.pl/">Digital Core Design</a>&nbsp;-
 private Intellectual Property (IP) Core provider and System-on-Chip
 (SoC) design house<br />
-      <a href="http://microcontrollershop.com/">MicroController
-Pros
+      <a href="http://microcontrollershop.com/">MicroController Pros
 Corporation (&micro;CPros)</a> - Development Tools for All
 Major Microcontroller Architectures<br />
-      <a href="http://www.soudez.be/">www.soudez.be (unrelated link?)</a>
+      <a href="http://www.soudez.be/">www.soudez.be</a>
 -&nbsp;<span class="genmed"><font face="Arial, sans-serif"><span style="text-decoration: none;">DS89C450</span></font></span>
 system over USB port in a standard DIL40 package<br />
-      <a href="http://www.proware.ind.br">www.proware.ind.br</a>
+      <!-- <a href="http://www.proware.ind.br">www.proware.ind.br</a>
 -&nbsp;<span class="genmed"><font face="Arial, sans-serif"><span style="text-decoration: none;">8051</span></font></span>
-development board with SDCC library code<br />
+development board with SDCC library code<br /> -->
       </p>
+
       <h2>Links - Compilers, Assemblers, Linkers</h2>
       <p>
       <a href="http://shop-pdp.kent.edu/ashtml/asxxxx.htm">ASxxxx Cross Assemblers</a> - assemblers and linkers from which SDCC derived
@@ -78,13 +79,19 @@ snippets.<br />
       <a href="http://www.freertos.org/">FreeRTOS</a>
 - a portable, open source, mini Real Time Kernel<br />
       </p>
+
+      <h2>Links - SDCC IDEs</h2>
+      <p>
+      <a href="http://ktechlab.org/">KTechLab</a> - an Open Source Intergated Design Environment (IDE) for electronic and PIC microcontroller circuit design and simulation.<br />
+      <a href="http://www.bipom.com/products/us/319513.html">MicroIDE</a> - free IDE software to manage SDCC projects
+      </p>
+
       <h2>Links - Circuit Boards and EDA software</h2>
       <p>
-      <a href="http://www.geocities.com/pdmtr">PC
-board HOWTO using Press &amp; Peel Tech Film</a><br />
-      <a href="http://www.cadsoft.de">CadSoft</a> -
-makers of
-Eagle EDA software, download a free (somewhat restricted) version for
+      <!-- <a href="http://www.geocities.com/pdmtr">PC
+board HOWTO using Press &amp; Peel Tech Film</a><br /> -->
+      <a href="http://www.gipsa-lab.inpg.fr/realise_au_lis/kicad/index.html">Kicad</a> - Free Open Source PCB Schematic Capture and Board Design<br />
+      <a href="http://www.cadsoft.de">CadSoft</a> - makers of Eagle EDA software, download a free (somewhat restricted) version for
 Win32 or Linux.<br />
       </p>
       <h2>Links - IC Data and Searches</h2>
@@ -93,16 +100,21 @@ Win32 or Linux.<br />
 - This is just the most fabulous web site I have been to in a long
 time. Need info? Go here!<br />
       </p>
+
       <h2>Links - Embedded Ethernet</h2>
       <p>
-      <a href="mailto:werner.cornelius@gmx.de">Werner
-Cornelius</a>
-has a <a href="http://www.titro.de/ethernet/">web page</a>
+      <a href="mailto:werner.cornelius@gmx.de">Werner Cornelius</a>
+has a <a href="http://fiction-writing.de/index.php?lang=eng&pos=5&sub=4">web page</a>
 devoted to embedding the RealTek RTL8019AS ethernet controller chip
 into a microcontroller. Lots of small tips on wiring, what to do, and
 what not to do.<br />
       </p>
-      <h2>Links - LCD Stuff</h2>
+
+<!--  the lcd subdirectory magically disappeared (probably it was deleted by mistake) from the sdcc web page,
+      so the following links don't work any more.
+
+      I (borutr) sent a mail to Dave Helton 2012-02-28 asking him if he still have a backup copy of lcd dorectory -->
+      <!-- <h2>Links - LCD Stuff</h2>
       <p>
       <b>T6963 controlled LCD panels </b>(includes
 the Optrex DM-5000 series and others) </p>
@@ -122,21 +134,28 @@ winter of 2000-01 I will be updating the code and writing some docs.
 This may force me to write better code if I have to document it. Your
 feedbeed back on the code is welcome. If you have enhanced the code,
 made a better mouse trap... please email me and let me know.<br />
+      </p> -->
+
+      <h2>Links - Z80 based VoIP</h2>
+      <p>
+      <a href="http://palmmicro.com/ar1688/">AR1688</a> - Z80 based VoIP processor and module using SDCC compiler
       </p>
+
       <h2>Links - simulators</h2>
       <p>
-      <a href="http://www.dattalo.com/gnupic/gpsim.html">gpsim</a>
+      <a href="http://gpsim.sourceforge.net/">gpsim</a>
 - gpsim is a full-featured software simulator for Microchip PIC
 microcontrollers distributed under the GNU General Public License<br />
       <a href="http://mazsola.iit.uni-miskolc.hu/%7Edrdani/embedded/ucsim/">&micro;Csim</a>
 - the old ucsim page<br />
       </p>
+
       <hr />
       <p><i>If you have a link to an embedded
 application, either hardware or software, that you believe would be of
 interest to other readers, drop either myself or the list members a
 note.</i></p>
-      <p><a href="http://sourceforge.net/tracker/?func=add&amp;group_id=599&amp;atid=536150">Send
+      <p><a href="http://sourceforge.net/tracker/?group_id=599&atid=536150">Send
 us your link!</a></p>
 <?php include('./footer.php')?>
       </td>
