35eef335a5caf911efd67366efda885fe5f12faf
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-03-01 21:43:55 +0000

    * support/sdbinutils/binutils/version.c:   --vesion shown sd* names instead GNU *
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@7386 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/sdbinutils/binutils/version.c
+++ sdcc/support/sdbinutils/binutils/version.c
@@ -32,7 +32,9 @@ print_version (const char *name)
 {
   /* This output is intended to follow the GNU standards document.  */
   /* xgettext:c-format */
-  printf ("GNU %s %s\n", name, BFD_VERSION_STRING);
+  /* changed for sdbinutils */
+  /* printf ("GNU %s %s\n", name, BFD_VERSION_STRING); */
+  printf ("sd%s %s\n", name, BFD_VERSION_STRING);
   printf (_("Copyright 2011 Free Software Foundation, Inc.\n"));
   printf (_("\
 This program is free software; you may redistribute it under the terms of\n\
