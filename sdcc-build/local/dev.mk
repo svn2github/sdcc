25fa25e3501a53960b6f33f23e64ead30043759c
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-03-09 22:01:32 +0000

    - re-added explicit definitions of TARGETOS and HOSTOS
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@7418 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/dev.mk
+++ sdcc-build/local/dev.mk
@@ -1,6 +1,8 @@
 # Solaris on sparc
 # Dual processor
 #MAKEJOBFLAGS = -j 2
+TARGETOS = sparc-sun-solaris
+HOSTOS = sparc-sun-solaris
 
 # local includes and libraries
 TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
--- sdcc-build/local/solaris-x86.mk
+++ sdcc-build/local/solaris-x86.mk
@@ -1,6 +1,8 @@
 # Solaris on i386
 # Dual processor
 #MAKEJOBFLAGS = -j 2
+TARGETOS = i386-sun-solaris
+HOSTOS = i386-sun-solaris
 
 # local includes and libraries
 TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
