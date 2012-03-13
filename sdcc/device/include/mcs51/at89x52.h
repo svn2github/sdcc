6f756aa922165a63c5858b2e24bb66d87d7c0ead
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-03-13 10:17:10 +0000

    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@7442 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/device/include/mcs51/at89x52.h
+++ sdcc/device/include/mcs51/at89x52.h
@@ -8,7 +8,7 @@
 
    This library is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the
-   Free Software Foundation; either version 2.1, or (at your option) any
+   Free Software Foundation; either version 2, or (at your option) any
    later version.
 
    This library is distributed in the hope that it will be useful,
