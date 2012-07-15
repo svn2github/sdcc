0259268adbd92c5f34cc70c09281556fb894f923
MaartenBrock <MaartenBrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-07-15 18:48:48 +0000

    * src/sdcc.vcxproj.filters, * src/sdcc.vcxproj: added SDCClospre.cc and SDCClospre.hpp * src/SDCClospre.cc (lospre): fixed warning
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8045 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/SDCClospre.cc
+++ sdcc/src/SDCClospre.cc
@@ -312,7 +312,7 @@ lospre (iCode *sic, ebbIndex *ebbi)
           if (safety && tree_dec_safety (tree_decomposition, control_flow_graph, ic) < 0)
             continue;
 
-          change += (tree_dec_lospre (tree_decomposition, control_flow_graph, ic) > 0);
+          change |= (tree_dec_lospre (tree_decomposition, control_flow_graph, ic) > 0);
         }
     }
 }
