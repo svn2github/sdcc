03b65ec6e8e97154bed2716e555ec127fdb4ab89
MaartenBrock <MaartenBrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-07-21 09:51:09 +0000

    * support/regression/tests/gcc-torture-execute-930513-1.c: modified to   accept "<NO FLOAT>" and prevent buffer overrun
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8052 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-930513-1.c
+++ sdcc/support/regression/tests/gcc-torture-execute-930513-1.c
@@ -3,13 +3,9 @@
  */
 
 #include <testfwk.h>
-
-#ifdef __SDCC
-#pragma std_c99
-#endif
-
 #include <stdio.h>
-char buf[2];
+
+char buf[12];
 
 void f (int (*fp)(char *, const char *, ...))
 {
@@ -19,11 +15,7 @@ void f (int (*fp)(char *, const char *, ...))
 void
 testTortureExecute (void)
 {
-#if 0
   f (&sprintf);
-  if (buf[0] != '5' || buf[1] != 0)
-    ASSERT (0);
-  return;
-#endif
+  ASSERT ((buf[0] == '<' && buf[1] == 'N') ||   // "<NO FLOAT>""
+          (buf[0] == '5' && buf[1] == 0));      // "5"
 }
-
