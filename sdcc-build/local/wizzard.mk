0abdf369b44964b041409bc35ca0e0f3fc184557
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-08-08 21:36:38 +0000

    * local/wizzard.mk:   created for Rasbian (Debian on Raspberry Pi) ARMv6 armhf machine
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8066 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc-build/local/wizzard.mk
@@ -0,0 +1,14 @@
+# Generate ARMv6 armhf Linux executables on
+# Debian (Raspbian) on Raspberry Pi
+
+# Spawn 2 make proceses
+MAKEJOBFLAGS = -j 2
+
+HOSTOS = armv6l-unknown-linux-gnueabihf
+TARGETOS = armv6l-unknown-linux-gnueabihf
+
+# local includes and libraries
+TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCPPFLAGS = "-I$(HOME)/local-$(HOSTNAME)/include"
+TARGETLDFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
