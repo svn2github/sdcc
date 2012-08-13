994ebcc2ee0e1ce5df0fb5bac665e12e169f68c0
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-08-13 11:04:28 +0000

    * local/wizzard.mk: use -fsigned-char since ARM default is unsigned char
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8071 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/local/wizzard.mk
+++ sdcc-build/local/wizzard.mk
@@ -8,7 +8,7 @@ HOSTOS = armv6l-unknown-linux-gnueabihf
 TARGETOS = armv6l-unknown-linux-gnueabihf
 
 # local includes and libraries
-TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
-TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCXXFLAGS = "-O2 -fsigned-char -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCFLAGS = "-O2 -fsigned-char -I$(HOME)/local-$(HOSTNAME)/include"
 TARGETCPPFLAGS = "-I$(HOME)/local-$(HOSTNAME)/include"
 TARGETLDFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
