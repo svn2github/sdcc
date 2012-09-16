94722ad8433b6066aeff963a3e2b9850870d2bf2
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-09-16 13:37:00 +0000

    Reduced array size, reenabled test.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8107 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-20030209-1.c
+++ sdcc/support/regression/tests/gcc-torture-execute-20030209-1.c
@@ -10,31 +10,21 @@
 #endif
 
 #ifndef __SDCC_mcs51
-#ifndef __SDCC_gbz80
-#ifndef __SDCC_hc08
-#ifndef __SDCC_s08
-double x[100][100]; /* Too big for mcs51 and gbz80. */
+double x[10][10]; /* Was [100][100] originally, while the 9 below was 99, but that was too big for sdcc targets. */
 #endif
-#endif
-#endif
-#endif
+
 void
 testTortureExecute (void)
 {
 #ifndef __SDCC_mcs51
-#ifndef __SDCC_gbz80
-#ifndef __SDCC_hc08
-#ifndef __SDCC_s08
   int i;
 
-  i = 99;
+  i = 9;
   x[i][0] = 42;
-  if (x[99][0] != 42)
+  if (x[9][0] != 42)
     ASSERT (0);
   return;
 #endif
-#endif
-#endif
-#endif
+
 }
 
