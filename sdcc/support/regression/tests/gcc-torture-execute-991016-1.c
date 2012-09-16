2562bd19c566166a55e5c2bbe16b68cd29e739c0
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-09-16 13:44:30 +0000

    Enabled test for targets that support long long, since returning long long was implemented some time ago.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8108 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-991016-1.c
+++ sdcc/support/regression/tests/gcc-torture-execute-991016-1.c
@@ -12,8 +12,7 @@
    mode but have different alias sets.  DOIT tries to get gcse to
    invalidly hoist one of the values out of the loop.  */
 
-#if 0
-//long long is not supported for some targets, and on the targets that support long long, this test crashes sdcc.
+#if !defined(__SDCC_mcs51) && !defined(__SDCC_ds390) && !defined(__SDCC_pic14) && !defined(__SDCC_pic16)
 typedef int T0;
 typedef long T1;
 typedef long long T2;
@@ -54,7 +53,7 @@ doit(int sel, int n, void *p)
 void
 testTortureExecute (void)
 {
-#if 0
+#if !defined(__SDCC_mcs51) && !defined(__SDCC_ds390) && !defined(__SDCC_pic14) && !defined(__SDCC_pic16)
   T0 v0; T1 v1; T2 v2;
 
   v0 = 1; doit(0, 5, &v0);
