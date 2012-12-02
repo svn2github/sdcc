7ba0d4ea2f8c099de02306e54664593bda2bfddd
molnarkaroly <molnarkaroly@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-12-02 11:16:16 +0000

    * support/scripts/cinc2h.pl,   support/scripts/pic14-header-parser.pl,   support/scripts/pic16-header-parser.pl: I changed the copyright.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8233 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/scripts/pic14-header-parser.pl
+++ sdcc/support/scripts/pic14-header-parser.pl
@@ -4,20 +4,27 @@
 
   Copyright (C) 2012 Molnar Karoly <molnarkaroly@users.sf.net>
 
-  This library is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published by the
-  Free Software Foundation; either version 2, or (at your option) any
-  later version.
+    This file is part of SDCC. 
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
+    This software is provided 'as-is', without any express or implied
+    warranty.  In no event will the authors be held liable for any damages
+    arising from the use of this software.
 
-  You should have received a copy of the GNU General Public License
-  along with this library; see the file COPYING. If not, write to the
-  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
-  MA 02110-1301, USA.
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+       claim that you wrote the original software. If you use this software
+       in a product, an acknowledgment in the product documentation would be
+       appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source distribution.
+
+================================================================================
 
     Proposal for use: ./pic14-header-parser.pl -gp
 
