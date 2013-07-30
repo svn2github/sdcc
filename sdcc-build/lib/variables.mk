84e32b1cf43863e53882d2cc3e5b03d37307132e
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2013-07-30 13:03:34 +0000

    Enable stm8 regression testing.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8791 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/lib/variables.mk
+++ sdcc-build/lib/variables.mk
@@ -58,8 +58,8 @@ SVN_ROOT = svn://svn.code.sf.net/p/sdcc/code
 #  REGTESTTARGETS0: These will always be tested
 #  REGTESTTARGETS1: These will be tested on even days
 #  REGTESTTARGETS2: These will be tested on odd days
-REGTESTTARGETS0 = test-mcs51-small test-hc08 test-ds390
-REGTESTTARGETS1 = test-mcs51-large test-ucr3ka test-ucgbz80 test-ucz80
+REGTESTTARGETS0 = test-mcs51-small test-ds390 test-stm8
+REGTESTTARGETS1 = test-mcs51-large test-hc08 test-ucr3ka test-ucgbz80 test-ucz80
 REGTESTTARGETS2 = test-mcs51-stack-auto test-s08 test-ucr2k test-ucz180
 
 DAYODD = $(shell date +%j | awk '{print $$0%2}')
