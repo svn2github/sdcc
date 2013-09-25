2f3ba268a6df9dd740350ce2c1f1007d1fdb95b4
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2013-09-25 08:48:05 +0000

    * packages/build.bison/Makefile: upgrade to version 3.0 * packages/build.flex/Makefile: upgrade to version 2.5.37 * packages/packages.mk: cosmetic changes
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8879 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-cf/packages/packages.mk
+++ sdcc-cf/packages/packages.mk
@@ -1,10 +1,10 @@
 HOSTNAME := $(shell if [ $(shell expr $(shell hostname) : '.*\.') != '0' ]; then expr $(shell hostname) : '\([^.]*\).'; else echo $(shell hostname); fi)
-LOCAL_DIR=$(HOME)/local-$(HOSTNAME)
+LOCAL_DIR = $(HOME)/local-$(HOSTNAME)
 
 OS = $(shell uname)
 
-isTTY=[ -t 1 -o -S /dev/stdin ]
-WGET=if $(isTTY); then WGET_FLAGS='-P'; else WGET_FLAGS='-nv -P'; fi; wget $$WGET_FLAGS download
+isTTY = [ -t 1 -o -S /dev/stdin ]
+WGET = if $(isTTY); then WGET_FLAGS = '-P'; else WGET_FLAGS = '-nv -P'; fi; wget $$WGET_FLAGS download
 
 CFLAGS += -I$(HOME)/local-$(HOSTNAME)/include
 CXXFLAGS += -I$(HOME)/local-$(HOSTNAME)/include
