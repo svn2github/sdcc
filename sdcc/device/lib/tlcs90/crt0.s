4f6bb20b0791b6ef10ad3b296a86ca21635eecdd
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2013-12-18 10:30:33 +0000

    crt0 for tlcs90.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8909 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/device/lib/tlcs90/crt0.s
+++ sdcc/device/lib/tlcs90/crt0.s
@@ -1,64 +1,431 @@
-;--------------------------------------------------------------------------
-;  crt0.s - Generic crt0.s for a Z80
-;
-;  Copyright (C) 2000, Michael Hope
-;
-;  This library is free software; you can redistribute it and/or modify it
-;  under the terms of the GNU General Public License as published by the
-;  Free Software Foundation; either version 2, or (at your option) any
-;  later version.
-;
-;  This library is distributed in the hope that it will be useful,
-;  but WITHOUT ANY WARRANTY; without even the implied warranty of
-;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;  GNU General Public License for more details.
-;
-;  You should have received a copy of the GNU General Public License 
-;  along with this library; see the file COPYING. If not, write to the
-;  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
-;   MA 02110-1301, USA.
-;
-;  As a special exception, if you link this library with other files,
-;  some of which are compiled with SDCC, to produce an executable,
-;  this library does not by itself cause the resulting executable to
-;  be covered by the GNU General Public License. This exception does
-;  not however invalidate any other reasons why the executable file
-;   might be covered by the GNU General Public License.
-;--------------------------------------------------------------------------
-
+;; crt0_asm.s - tlcs90
+	
+	.t90
+	
 	.module crt0
-	.globl	_main
 
+	.globl _meminit
+	.globl _main
+	.globl _xxaddr
+	.globl _xxby
+	.globl _xxdata
+
+	.globl _ser_putchar_irq
+	.globl _ser_puts
+	
+	;; put as abs in dat at end of file..
+	;.globl _ser_irq_flags
+	;.globl _ser_char
+	
+	.globl _xget_byte_asm
+	.globl _xset_byte_asm
+
+
+	 ;; for interrupts the pc and af are pushed on the stack
+	
+	.globl _irq_info_swi
+	.globl _irq_info_intwd
+	.globl _irq_info_intt0
+	.globl _irq_info_intt1
+	.globl _timer1_irq
+	.globl _irq_info_intt3
+	.globl _irq_info_intt4
+	.globl _irq_info_int1
+	.globl _irq_info_intt5
+
+	.globl _ticks16
+	.globl _xflag
+	
+	;; NOTE: header might get trashed by linker when code loc ist not used!?
 	.area	_HEADER (ABS)
-	;; Reset vector
+	
 	.org 	0
-	jp	init
 
-	.org	0x08
-	reti
-	.org	0x10
-	reti
-	.org	0x18
-	reti
-	.org	0x20
-	reti
-	.org	0x28
-	reti
-	.org	0x30
-	reti
-	.org	0x38
-	reti
+	.include "t90regs.s"
 
-	.org	0x100
-init:
-	;; Stack at the top of memory.
-	ld	sp,#0xffff
+	;; _ser_irq_flags
+TX_ACTIVE	.equ 0
+RX_READY        .equ 1
+	
+L0:
+	;halt
+	;jr    L0
+	jp    _astart
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L10:	
+	; SWI
+	jp    _swi_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
 
+L18:	
+	nop  ; NMI
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+
+L20:
+	; INTWD
+	jp    _intwd_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L28:	
+	reti ; INT0
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+L30:	
+	; INTT0
+	jp    _intt0_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L38:	
+	; INTT1
+	jp    _intt1_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L40:
+	; INTAD 
+	reti 
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L48:	
+	; INTT3
+	jp    _intt3_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L50:	
+	; INTT4
+	jp    _intt4_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L58:	
+	; INT1
+	jp    _int1_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L60:	
+	; INTT5
+	jp    _intt5_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L68:	
+	reti ; INT2
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L70:	
+	jp _serial_rx_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L78:	
+	jp _serial_tx_irq
+	nop  
+	nop  
+	nop  
+	nop  
+	nop
+L80:	
+	nop  
+
+	.org 0x100
+	
+_astart:
+	ld    sp, #0x0FFA0	; stack
+	
+	;halt
+	;swi
+	
+	or    (P01CR), #0x04
+	
+	ld    (P3), #0xF7	
+	ld    (P3CR), #0xA5	; P30 = RXD, P31 = IN:PaperOut CN3, P32 = TXD, P33 = OUT:CN2.LF P34:IN, P37:IN
+	
+	ld    (P4), #0x00
+	ld    (P4CR), #0x03	; address output
+	ld    (P7), #0x03
+	ld    (P67CR), #0xF0
+	ld    (SMMOD), #0xA0
+	ld    (SMCR), #0x00
+	ld    (T4MOD), #0x35
+	ld    (P8), #0x08
+	ld    (P8CR), #0x08
+	ld    (T4FFCR), #0x01
+	
+	ld    (TMOD), #0x40
+	
+	;ld    (SCMOD), #0x03	; 9600
+	ld    (SCMOD), #0x01	; 19200
+	or    (SCMOD), #0x08	; 8-bit data
+	or    (SCMOD), #0x20	; receiver enable
+
+	ld    (SCCR), #0x00
+	
+	ld    (WDMOD), #0xC0
+	ld    (TRUN), #0x20	; PRRUN, start prescaler
+	or    (TRUN), #0xE0	; BRATE, 19200 baud
+
+	;; enable rx/tx irqs
+	set   0,(INTEL)		; tx
+	set   1,(INTEL)		; rx
+
+	;call _boot1
+	
+	ld    hl, #0xFF20	; data start
+	ld    a, #0x00
+	
+clear_ram:
+	call  _wd_reset_asm
+	
+	ld    (hl),a
+	inc   hl
+	cp    hl, #0xFFC0	; io start, 160 bytes data
+	jr    nz, clear_ram
+	
+	call  _wd_reset_asm
+
+	res   TX_ACTIVE,(_ser_irq_flags)
+	
+	;call _boot2
+	call _meminit
+	
+	call  _wd_reset_asm
+	
         ;; Initialise global variables
-        call    gsinit
-	call	_main
-	jp	_exit
 
+	;call _boot3
+	
+        call gsinit
+
+	;call _boot4
+	
+	call _main
+	halt
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	.area _CODE
+	
+	;; get far byte using by
+	
+_xget_byte_asm:
+	push bc
+	ld	iy, (_xxaddr)
+	ld	b, (BY)
+	ld	a, (_xxby)
+	ld	(BY), a
+	ld	a,(iy)
+	ld	(BY), b
+	ld	(_xxdata), a
+	pop	bc
+	ret
+
+_xset_byte_asm:
+	push bc
+	ld	iy, (_xxaddr)
+	ld	b, (BY)
+	ld	a, (_xxby)
+	ld	(BY), a
+	ld 	a, (_xxdata)
+	ld	(iy), a
+	ld	(BY), b
+	pop	bc
+	ret
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+_serial_rx_irq:
+	ld a, (SCBUF)
+	ld (_ser_char), a
+	set RX_READY, (_ser_irq_flags)
+	reti 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+_serial_tx_irq:
+	res TX_ACTIVE,(_ser_irq_flags)
+	ld (IRFH), #0x0F ; clear tx request flag (vector 0x78/8 = 0x0F)
+	reti 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+_swi_irq:
+	push hl
+	ld hl, 4(sp)
+	ld (_swi_pc), hl	; pc
+	ld hl, 2(sp)
+	ld (_swi_af), hl	; af
+	call _irq_info_swi
+	pop hl
+	reti
+_intwd_irq:	
+	call _irq_info_intwd
+	reti
+_intt0_irq:	
+	call _irq_info_intt0
+	reti
+_intt1_irq:	
+	;call _irq_info_intt1
+	;exx
+
+	; hangs when exx is used?
+	incw (_ticks16) 
+
+	; hangs when exx is used?
+	;; ld hl, (_ticks16)
+	;; inc hl
+	;; ld (_ticks16), hl
+	
+	ld (WDCR), #0x4E
+	ld (IRFL), #0x10;
+	;call _timer1_irq
+	;exx
+	reti
+_intt3_irq:	
+	call _irq_info_intt3
+	reti
+_intt4_irq:	
+	call _irq_info_intt4
+	reti
+_int1_irq:	
+	call _irq_info_int1
+	reti
+_intt5_irq:	
+	call _irq_info_intt5
+	reti
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+_ser_putchar_irq:
+	ld    a, 2(sp)
+
+_ser_putchar_asm:
+	
+loop1:
+
+	;; check tx active flag
+	bit   TX_ACTIVE,(_ser_irq_flags)
+	jr    z, send1
+
+	di
+	bit   0,(IRFH) ; check irq
+	jr    nz, send1
+	ei
+	
+	nop
+	nop
+	call  _wd_reset_asm
+	jr    loop1 ; loop, wait for irq
+
+send1:	
+	di
+	ld    (IRFH),#0x0F ; clear tx request flag (vector 0x78/8 = 0x0F)
+	ld    (SCBUF),a ; send
+	set   TX_ACTIVE,(_ser_irq_flags) ; tx active..
+	ei
+	ret
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+_ser_puts:
+	ld hl, 2(sp)
+	
+ser_puts1:
+	ld a,(hl)
+	or a, a
+	ret z
+	
+	call _ser_putchar_asm
+	inc hl
+	jr ser_puts1
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+_WatchDogDisable:
+	ld    (WDMOD), #0x00 	; wd disable code
+	ld    (WDCR), #0xB1 	; wd disable code
+	ret
+	
+_wd_reset_asm:
+	ld    (WDCR), #0x4E 	; wd clear code
+
+.if 0
+	; STAR ASIC? Not needed..
+	push  bc
+	push  iy
+	
+	ld    b,(BY)
+	ld    (BY), #0x01
+	ld    iy, #0x4001		
+	set   1,(iy)
+	nop  
+	nop  
+	res   1,(iy)
+	ld    (BY),b
+	
+	pop   iy
+	pop   bc
+
+.endif
+	
+	ret
+	
 	;; Ordering of segments for the linker.
 	.area	_HOME
 	.area	_CODE
@@ -73,18 +440,6 @@ init:
 	.area   _HEAP
 
 	.area   _CODE
-__clock::
-	ld	a,#2
-	rst     0x08
-	ret
-
-_exit::
-	;; Exit - special code to the emulator
-	ld	a,#0
-	rst     0x08
-1$:
-	halt
-	jr	1$
 
 	.area   _GSINIT
 gsinit::
@@ -99,4 +454,13 @@ gsinit_next:
 
 	.area   _GSFINAL
 	ret
-
+	
+	.area _DATA
+; __sfr __at	
+_ser_irq_flags	=	0xffbf
+_ser_char	=	0xffbe
+_swi_pc		=       0xffbc
+_swi_af         =       0xffba
+_ticks16	=	0xffb8
+_xflag		=	0xffb7
+	
