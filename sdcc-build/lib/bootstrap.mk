c1b91584e00c7b421d99ffd45468162ef55b309b
epetrich <epetrich@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2013-12-26 21:03:08 +0000

    * lib/bootstrap.mk: copy either sjlj or seh support dll
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@8928 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/lib/bootstrap.mk
+++ sdcc-build/lib/bootstrap.mk
@@ -57,7 +57,7 @@ ifeq ($(TARGETOS), x86_64-w64-mingw32)
 # Windows 64bit:
 # include readline5.dll in the package
 	cp $(HOME)/local-$(HOSTNAME)/cross-tools-w64/x86_64-w64-mingw32/dll/readline5.dll $(BUILDDIR)$(PREFIX)/bin
-	cp $(HOME)/local-$(HOSTNAME)/cross-tools-w64/bin/libgcc_s_sjlj-1.dll $(BUILDDIR)$(PREFIX)/bin
+	cp $(HOME)/local-$(HOSTNAME)/cross-tools-w64/bin/libgcc_s_*-1.dll $(BUILDDIR)$(PREFIX)/bin
 	cp $(HOME)/local-$(HOSTNAME)/cross-tools-w64/bin/libstdc++-6.dll $(BUILDDIR)$(PREFIX)/bin
 else
 # Windows 32bit:
