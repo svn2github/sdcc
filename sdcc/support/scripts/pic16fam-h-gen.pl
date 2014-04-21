6946ba0cdb7cbe991dda061cd2f962a52d7f1d01
molnarkaroly <molnarkaroly@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2014-04-21 08:16:02 +0000

    * sdcc/support/scripts/pic16fam-h-gen.pl: updated
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9001 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/scripts/pic16fam-h-gen.pl
+++ sdcc/support/scripts/pic16fam-h-gen.pl
@@ -1,39 +1,42 @@
 #!/usr/bin/perl -w
 
-#
-# This script generates a C header file that maps the target device (as
-# indicated via the sdcc generated -Dpic16cxxx or -Dpic12fxxx or -Dpic16fxxx
-# macro) to its device family and the device families to their respective
-# style of ADC, USART and SSP programming for use in the SDCC PIC14 I/O library.
-#
-# Copyright 2010 Raphael Neider <rneider AT web.de>
-# Very modified and expanded to the pic14 series: Molnár Károly, 2012 <molnarkaroly@users.sf.net>
-#
-# This file is part of SDCC.
-# 
-# SDCC is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 2 of the License, or (at your
-# option) any later version.
-#
-# SDCC is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with SDCC.  If not, see <http://www.gnu.org/licenses/>.
-#
+=back
 
-#
-# Usage: perl pic16fam-h-gen.pl [-i|-p|-h]
-#
-# This will create pic16fam.h.gen in your current directory.
-# Check sanity of the file and move it to .../include/pic14/pic16fam.h.
-# If you assigned new I/O styles, implement them in
-# .../include/pic14/{adc,i2c,pwm,spi,usart}.h and
-# .../lib/pic14/libio/*/*.c
-#
+    This script generates a C header file that maps the target device (as
+    indicated via the sdcc generated -Dpic16cxxx or -Dpic12fxxx or -Dpic16fxxx
+    macro) to its device family and the device families to their respective
+    style of ADC, USART and SSP programming for use in the SDCC PIC14 I/O library.
+
+    Copyright 2010 Raphael Neider <rneider AT web.de>
+    PIC14 port:
+    Copyright 2012-2014 Molnar Karoly <molnarkaroly@users.sf.net>
+
+    This file is part of SDCC.
+
+    SDCC is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 2 of the License, or (at your
+    option) any later version.
+
+    SDCC is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SDCC.  If not, see <http://www.gnu.org/licenses/>.
+
+    Usage: perl pic16fam-h-gen.pl [-i|-p|-h]
+
+	This will create pic16fam.h.gen in your current directory.
+	Check sanity of the file and move it to .../include/pic14/pic16fam.h.
+	If you assigned new I/O styles, implement them in
+	.../include/pic14/{adc,i2c,pwm,spi,usart}.h and
+	.../lib/pic14/libio/*/*.c
+
+    $Id$
+=cut
+
 
 use strict;
 use 5.10.1;
@@ -410,22 +413,24 @@ __END__
     SECTION=REGULAR
 
 10f320,10f322
-12f609,12f615,12f617,12f629,12f635,12f675,12f683,12f752
+12f609,12f615,12f617,12f629,12f635,12f675,12f683
+12f752
 16c62,16c63a,16c65b,16c71,16c72,16c73b,16c74b
 16c432,16c433
 16c554,16c557,16c558
 16c620,16c620a,16c621,16c621a,16c622,16c622a
 16c710,16c711,16c715,16c717,16c745,16c765,16c770,16c771,16c773,16c774,16c781,16c782
 16c925,16c926
+16cr73,16cr74,16cr76,16cr77
+16cr620a
 16f72,16f73,16f74,16f76,16f77
 16f84,16f84a,16f87,16f88
-16f610,16f616,16f627,16f627a,16f628,16f628a,16f630,16f631,16f636,16f639,16f648a
-16f676,16f677,16f684,16f685,16f687,16f688,16f689,16f690
-16f707,16f716,16f720,16f721,16f722,16f722a,16f723,16f723a,16f724,16f726,16f727,16f737,16f747,16f767,16f777,16f785
+16f610,16f616,16f627,16f627a,16f628,16f628a,16f630,16f631,16f636,16f639,16f648,16f648a,16f676,16f677,16f684,16f685,16f687,16f688,16f689,16f690
+16f707,16f716,16f720,16f721,16f722,16f722a,16f723,16f723a,16f724,16f726,16f727,16f737,16f747,16f753,16f767,16f777,16f785
 16f818,16f819,16f870,16f871,16f872,16f873,16f873a,16f874,16f874a,16f876,16f876a,16f877,16f877a
 16f882,16f883,16f884,16f886,16f887
 16f913,16f914,16f916,16f917,16f946
-16hv616
+16hv610,16hv616,16hv753,16hv785
 
 ################################################################################
 #
@@ -435,10 +440,14 @@ __END__
 
     SECTION=ENHANCED
 
-12f1501,12lf1552,12f1822,12f1840
+12f1501,12f1571,12f1572
+12f1612
+12f1822,12f1840
+12lf1552
 16f1454,16f1455,16f1458,16f1459
 16f1503,16f1507,16f1508,16f1509,16f1512,16f1513,16f1516,16f1517,16f1518,16f1519,16f1526,16f1527
-16f1782,16f1783,16f1784,16f1786,16f1787
+16f1613
+16f1703,16f1704,16f1705,16f1707,16f1708,16f1709,16f1713,16f1716,16f1717,16f1718,16f1719,16f1782,16f1783,16f1784,16f1786,16f1787,16f1788,16f1789
 16f1823,16f1824,16f1825,16f1826,16f1827,16f1828,16f1829,16f1847
 16f1933,16f1934,16f1936,16f1937,16f1938,16f1939,16f1946,16f1947
 16lf1902,16lf1903,16lf1904,16lf1906,16lf1907
@@ -499,7 +508,8 @@ __END__
 1607241:16f724,16f727
 1607371:16f737,16f767
 1607471:16f747,16f777
-1607851:16f785
+1607531:16f753,16hv753
+1607851:16f785,16hv785
 1608181:16f818,16f819
 1608711:16f871,16f874,16f874a,16f877,16f877a
 1608821:16f882,16f883,16f886
@@ -507,6 +517,8 @@ __END__
 1609131:16f913,16f916
 1609141:16f914,16f917,16f946
 1215011:12f1501
+1215711:12f1571,12f1572
+1216121:12f1612
 1218221:12f1822,12f1840
 1614551:16f1455
 1614591:16f1459
@@ -515,8 +527,18 @@ __END__
 1615121:16f1512,16f1513,16f1516,16f1518
 1615171:16f1517,16f1519
 1615261:16f1526,16f1527
+1616131:16f1613
+1617031:16f1703
+1617041:16f1704,16f1705
+1617071:16f1707
+1617081:16f1708
+1617091:16f1709
+1617131:16f1713,16f1716,16f1718
+1617171:16f1717,16f1719
 1617821:16f1782,16f1783,16f1786
 1617841:16f1784,16f1787
+1617881:16f1788
+1617891:16f1789
 1618231:16f1823
 1618241:16f1824,16f1825
 1618261:16f1826,16f1827,16f1847
@@ -545,19 +567,32 @@ __END__
 1607222:16f722,16f722a,16f723,16f723a,16f724,16f726,16f727
 1607202:16f720,16f721,16f913,16f916
 1607372:16f737,16f747,16f767,16f777
-1607852:16f785
+1607532:16f753,16hv753
+1607852:16f785,16hv785
 1608182:16f818,16f819
 1609142:16f914,16f917,16f946
 1215012:12f1501
+1215712:12f1571,12f1572
+1216122:12f1612
 1218222:12f1822,12f1840
 1614542:16f1454,16f1455
 1614592:16f1459
 1615032:16f1503,16f1507,16f1508,16f1509
 1615122:16f1512,16f1513,16f1516,16f1517,16f1518,16f1519
 1615262:16f1526,16f1527
+1616132:16f1613
+1617032:16f1703
+1617042:16f1704,16f1705
+1617072:16f1707
+1617082:16f1708
+1617092:16f1709
+1617132:16f1713,16f1716,16f1718
+1617172:16f1717,16f1719
 1617822:16f1782,16f1783
 1617842:16f1784,16f1787
 1617862:16f1786
+1617882:16f1788
+1617892:16f1789
 1618232:16f1823
 1618242:16f1824,16f1825
 1618262:16f1826
@@ -592,7 +627,16 @@ __END__
 1615083:16f1508,16f1509
 1615123:16f1512,16f1513,16f1516,16f1517,16f1518,16f1519
 1615263:16f1526,16f1527
+1617033:16f1703
+1617043:16f1704,16f1705
+1617073:16f1707
+1617083:16f1708
+1617093:16f1709
+1617133:16f1713,16f1716,16f1718
+1617173:16f1717,16f1719
 1617823:16f1782,16f1783,16f1784,16f1786,16f1787
+1617883:16f1788
+1617893:16f1789
 1618233:16f1823,16f1824,16f1825
 1618263:16f1826
 1618273:16f1827,16f1847
@@ -612,13 +656,22 @@ __END__
 1607204:16f720,16f721
 1607734:16c773,16c774,16f722,16f722a,16f723,16f723a,16f724,16f726,16f727,16f737,16f747,16f767,16f777,16f870,16f871,16f873,16f873a,16f874,16f874a,16f876,16f876a,16f877,16f877a,16f913,16f914,16f916,16f917,16f946
 1608824:16f882,16f883,16f884,16f886,16f887
+1215724:12f1572
 1218224:12f1822,12f1840
 1614544:16f1454,16f1455
 1614594:16f1459
 1615084:16f1508,16f1509
 1615124:16f1512,16f1513,16f1516,16f1517,16f1518,16f1519
 1615264:16f1526,16f1527
+1617044:16f1704,16f1705
+1617074:16f1707
+1617084:16f1708
+1617094:16f1709
+1617134:16f1713,16f1716,16f1718
+1617174:16f1717,16f1719
 1617824:16f1782,16f1783,16f1784,16f1786,16f1787
+1617884:16f1788
+1617894:16f1789
 1618234:16f1823,16f1824,16f1825
 1618264:16f1826,16f1827,16f1847
 1618284:16f1828,16f1829
