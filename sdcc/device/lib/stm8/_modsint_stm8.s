4e3bb13701b7c7e6b857a6a9cf86741a54110982
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2014-09-22 07:03:51 +0000

    Add more stm8 optimized functions.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9076 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc/device/lib/stm8/_modsint_stm8.s
@@ -0,0 +1,50 @@
+;--------------------------------------------------------------------------
+;  _modsint_stm8.s
+;
+;  Copyright (C) 2014, Krzysztof Nikiel, Ben Shi
+;
+;  This library is free software; you can redistribute it and/or modify it
+;  under the terms of the GNU General Public License as published by the
+;  Free Software Foundation; either version 2, or (at your option) any
+;  later version.
+;
+;  This library is distributed in the hope that it will be useful,
+;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;  GNU General Public License for more details.
+;
+;  You should have received a copy of the GNU General Public License 
+;  along with this library; see the file COPYING. If not, write to the
+;  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+;   MA 02110-1301, USA.
+;
+;  As a special exception, if you link this library with other files,
+;  some of which are compiled with SDCC, to produce an executable,
+;  this library does not by itself cause the resulting executable to
+;  be covered by the GNU General Public License. This exception does
+;  not however invalidate any other reasons why the executable file
+;   might be covered by the GNU General Public License.
+;--------------------------------------------------------------------------
+
+	.globl __modsint_stm8
+
+	.area CODE
+__modsint_stm8:
+	ldw	x, (#3, sp)
+	ldw	y, (#5, sp)
+	ld	a, xh
+	cpw	x, #0x0000
+	jrsge	__modsint_stm8_1
+	negw	x
+__modsint_stm8_1:
+	cpw	y, #0x0000
+	jrsge	__modsint_stm8_2
+	negw	y
+__modsint_stm8_2:
+	divw	x, y
+	and	a, #0x80
+	jreq	__modsint_stm8_3
+	negw	y
+__modsint_stm8_3:
+	ldw	x, y
+	ret
