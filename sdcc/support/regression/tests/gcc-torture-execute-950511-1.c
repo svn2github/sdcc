6c5bf6e893f2d9453d7a861d80a31fc4c10d356c
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2014-12-03 05:02:02 +0000

    Disable gcc-torture-execute-950511-1.c
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9127 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-950511-1.c
+++ sdcc/support/regression/tests/gcc-torture-execute-950511-1.c
@@ -11,7 +11,9 @@
 void
 testTortureExecute (void)
 {
-#if !defined(__SDCC_mcs51) && !defined(__SDCC_ds390) && !defined(__SDCC_pic14) && !defined(__SDCC_pic16) && !defined(__SDCC_hc08) && !defined(__SDCC_s08) && !defined(__SDCC_ds400) && !defined(__SDCC_gbz80)
+#if !defined(__SDCC_mcs51) && !defined(__SDCC_ds390) && !defined(__SDCC_ds400) && !defined(__SDCC_pic14) && !defined(__SDCC_pic16) && !defined(__SDCC_hc08) && !defined(__SDCC_s08)
+#if !defined(__SDCC_stm8) && !defined(__SDCC_z80) && !defined(__SDCC_z180) && !defined(__SDCC_gbz80) && !defined(__SDCC_tlcs90) && !defined(__SDCC_r2k) && !defined(__SDCC_r3ka)
+
   unsigned long long xx;
   unsigned long long *x = (unsigned long long *) &xx;
 
@@ -20,6 +22,8 @@ testTortureExecute (void)
   if (*x != 9)
     ASSERT (0);
   return;
+
+#endif
 #endif
 }
 
