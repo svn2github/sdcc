3934350d07271f3e36e7193d88a4d963910aeb02
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2015-03-03 07:42:13 +0000

    * index.php: Modify Ben Shi's responsibility.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9190 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-web/index.php
+++ sdcc-web/index.php
@@ -219,7 +219,7 @@
                 sdasrab assembler and ucsim support</li>
               <li>Molnár Károly &lt;molnarkaroly.AT.users.sf.net&gt; - adding pic devices, developing and maintaining pic device files
                 generation scripts</li>
-              <li>Ben Shi &lt;powerstudio1st.AT.163.com&gt; - fixes bugs and improves the code quality of the STM8 port</li>
+              <li>Ben Shi &lt;powerstudio1st.AT.163.com&gt; - fixes bugs in the front end and improves the STM8 back end</li>
             </ul>
             <p>SDCC has had help from a number of external sources, including:</p>
             <ul>
