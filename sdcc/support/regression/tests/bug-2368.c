fecc29073f236a7746f5b44c16a904e67c2a9543
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2015-04-06 12:55:47 +0000

    Try to fix the failure of bug-2368.c in in test-host on sparc64-solaris.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9207 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/bug-2368.c
+++ sdcc/support/regression/tests/bug-2368.c
@@ -1,32 +1,52 @@
 /* bug-2368.c
    Wrong byte order in optimization of pointer assignment for stm8
  */
+
 #include <testfwk.h>
 #include <stdint.h>
 
+#ifdef __SDCC
+#pragma std_c99
+#endif
+
+#if defined (__SDCC_stm8) || defined (__SDCC_z80)
+
 #define TEST_VAL 0x1234
-#define loc ((volatile uint16_t *)0x7fd)
+#define loc ((volatile uint16_t *) 0x7fd)
 
 volatile uint16_t *p = loc;
 
-void testfunc1(void)
+void foo1 (void)
 {
-	*p = TEST_VAL;
+  *p = TEST_VAL;
 }
 
-void testfunc2(void)
+void foo2 (void)
 {
-	*loc = TEST_VAL;
+  *loc = TEST_VAL;
 }
 
+#endif
 
-void testBug(void)
+void testBug (void)
 {
-#if defined(__SDCC_stm8) || defined(__SDCC_z80)
-	testfunc1();
-	ASSERT (*loc == TEST_VAL);
-	testfunc2();
-	ASSERT (*loc == TEST_VAL);
+#if defined (__SDCC_stm8) || defined (__SDCC_z80)
+  volatile uint16_t *q = loc;
+
+  foo1 ();
+  ASSERT (*loc == TEST_VAL);
+  ASSERT (*p == TEST_VAL);
+  ASSERT (*q == TEST_VAL);
+
+  foo2 ();
+  ASSERT (*loc == TEST_VAL);
+  ASSERT (*p == TEST_VAL);
+  ASSERT (*q == TEST_VAL);
+
+  *q = TEST_VAL;
+  ASSERT (*loc == TEST_VAL);
+  ASSERT (*p == TEST_VAL);
+  ASSERT (*q == TEST_VAL);
+
 #endif
 }
-
