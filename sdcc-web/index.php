92a7951942ad9876cb5f0df853956d77073ca274
maartenbrock <maartenbrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2015-06-22 09:42:12 +0000

    * index.php: added News - sdcc 3.5.0 RC2
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9255 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-web/index.php
+++ sdcc-web/index.php
@@ -71,6 +71,12 @@
             <!-- START NEWS -->
             <h2><a name="News"></a>News</h2>
 
+            <p><i><b>June 20th, 2015: SDCC 3.5.0 RC2 released.</b></i></p>
+            <p>SDCC 3.5.0 Release Candidate 2 source, doc and binary packages for x86 Linux,
+              32 and 64 bit Windows and universal Mac OS X are available in corresponding folders at:
+              <a href="http://sourceforge.net/projects/sdcc/files/">
+              http://sourceforge.net/projects/sdcc/files/</a>.</p>
+
             <p><i><b>June 9th, 2015: SDCC 3.5.0 RC1 released.</b></i></p>
             <p>SDCC 3.5.0 Release Candidate 1 source, doc and binary packages for x86 Linux,
               32 and 64 bit Windows and universal Mac OS X are available in corresponding folders at:
