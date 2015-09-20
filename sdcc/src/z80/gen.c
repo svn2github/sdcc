8380b5dc66ef0e6796fdec927183463b90a878e5
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2015-09-20 04:51:32 +0000

    Fix bug #2333.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9322 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/z80/gen.c
+++ sdcc/src/z80/gen.c
@@ -7666,7 +7666,7 @@ genOr (const iCode * ic, iCode * ifx)
 {
   operand *left, *right, *result;
   int size, offset = 0;
-  unsigned long lit = 0L;
+  unsigned long long lit = 0;
   int bytelit = 0;
 
   aopOp ((left = IC_LEFT (ic)), ic, FALSE, FALSE);
@@ -7697,7 +7697,7 @@ genOr (const iCode * ic, iCode * ifx)
       left = tmp;
     }
   if (AOP_TYPE (right) == AOP_LIT)
-    lit = ulFromVal (AOP (right)->aopu.aop_lit);
+    lit = ullFromVal (AOP (right)->aopu.aop_lit);
 
   size = AOP_SIZE (result);
 
@@ -7835,7 +7835,7 @@ genXor (const iCode * ic, iCode * ifx)
 {
   operand *left, *right, *result;
   int size, offset = 0;
-  unsigned long lit = 0L;
+  unsigned long long lit = 0;
 
   aopOp ((left = IC_LEFT (ic)), ic, FALSE, FALSE);
   aopOp ((right = IC_RIGHT (ic)), ic, FALSE, FALSE);
@@ -7865,7 +7865,7 @@ genXor (const iCode * ic, iCode * ifx)
       left = tmp;
     }
   if (AOP_TYPE (right) == AOP_LIT)
-    lit = ulFromVal (AOP (right)->aopu.aop_lit);
+    lit = ullFromVal (AOP (right)->aopu.aop_lit);
 
   size = AOP_SIZE (result);
 
