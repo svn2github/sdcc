732c74a683972bde2764d02777d1a9f7b6033441
maartenbrock <maartenbrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2016-02-09 11:57:40 +0000

    * device/lib/Makefile.in: added and enabled large-mcs51-stack-auto * support/regression/ports/mcs51-large-stack-auto, * support/regression/ports/mcs51-large-stack-auto/spec.mk, * support/regression/Makefile.in: added test-mcs51-large-stack-auto * support/regression/tests/bug2094505.c: disabled for mcs51-large-stack-auto
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9501 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/bug2094505.c
+++ sdcc/support/regression/tests/bug2094505.c
@@ -7,7 +7,7 @@
 void
 testBug(void)
 {
-#if !defined(__SDCC) || !defined(__SDCC_mcs51) || defined(__SDCC_MODEL_LARGE)
+#if !defined(__SDCC) || !defined(__SDCC_mcs51) || (defined(__SDCC_MODEL_LARGE) && !defined(__SDCC_STACK_AUTO))
     // varA has to be declared for the bug to manifest itself
     volatile char varA[256] = {0};
     volatile unsigned int varB = 0x1;
