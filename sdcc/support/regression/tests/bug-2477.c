835824b9058f5f918c7578c28efe48a35afe4678
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2016-03-12 08:31:02 +0000

    Rollback to revision #9516.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9518 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/bug-2477.c
+++ sdcc/support/regression/tests/bug-2477.c
@@ -20,6 +20,8 @@ void
 testLongLong (void)
 {
 #if !defined(__SDCC_mcs51) && !defined(__SDCC_ds390) && !defined(__SDCC_ds400) && !defined(__SDCC_pic14) && !defined(__SDCC_pic16) && !defined(__SDCC_hc08) && !defined(__SDCC_s08)
+#if 0
   ASSERT (ss () == -1);
+#endif
 #endif
 }
