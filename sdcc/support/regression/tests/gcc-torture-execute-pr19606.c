dca25a0e014b1a6091059e969d446a4782b7dacf
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2016-03-31 06:21:48 +0000

    Improve test cases.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9531 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-pr19606.c
+++ sdcc/support/regression/tests/gcc-torture-execute-pr19606.c
@@ -33,7 +33,7 @@ void
 testTortureExecute (void)
 {
 #if !defined (__SDCC_mcs51) && !defined (__SDCC_ds390) && !defined (__SDCC_hc08) && !defined (__SDCC_s08)
-#if !(defined(PORT_HOST) && defined(__APPLE__)) // failed on MacOS in test-host
+#if !defined (PORT_HOST) // failed in test-host
   int r;
 
   r = foo ();
