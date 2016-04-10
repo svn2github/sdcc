f50c94e2c3f435a0815e4bf21456d5234942d78c
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2016-04-10 13:32:27 +0000

    Modify a test case.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9554 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/wchar.c
+++ sdcc/support/regression/tests/wchar.c
@@ -2,15 +2,20 @@
  */
 #include <testfwk.h>
 
+#if defined (__SDCC)
+
 #include <uchar.h>
 #include <wchar.h>
 #include <limits.h>
 
 mbstate_t ps;
 
+#endif
+
 static void
 testwchar(void)
 {
+#if defined (__SDCC)
 #if !defined(__SDCC_hc08) && !defined(__SDCC_s08)
 	wchar_t w;
 	char c[MB_LEN_MAX];
@@ -19,6 +24,7 @@ testwchar(void)
 	ASSERT(mbrtowc(&w, c, 1, &ps) == 1);
 	ASSERT(wcrtomb(c, w, &ps) == 1);
 	ASSERT(c[0] == 'C');
+#endif
 #endif
 }
 
