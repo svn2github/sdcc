fd765a01627c45328e93367a9d8ff3ad4e9bbcb2
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2016-06-03 12:27:42 +0000

    Work around an allocation issue.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9614 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/hc08/gen.c
+++ sdcc/src/hc08/gen.c
@@ -2180,7 +2180,7 @@ aopForSym (iCode * ic, symbol * sym, bool result)
     }
 
   /* if it is on the stack */
-  if (sym->onStack)
+  if (sym->onStack || (!IN_DIRSPACE (space) && !IN_FARSPACE (space))) /* The !IN_DIRSPACE (space) && !IN_FARSPACE (space) is just a workaround for bugs #2506, #2450. */
     {
       sym->aop = aop = newAsmop (AOP_SOF);
       aop->aopu.aop_dir = sym->rname;
@@ -2238,8 +2238,6 @@ aopForSym (iCode * ic, symbol * sym, bool result)
       aop->size = getSize (sym->type);
       return aop;
     }
-
-
 
   werror (E_INTERNAL_ERROR, __FILE__, __LINE__, "aopForSym should never reach here");
 
