d42a254b652634cd91d149a1c7c8308c2b19166a
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2016-07-29 14:45:40 +0000

    (c) message updates
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9687 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/sim/ucsim/pstr.h
+++ sdcc/sim/ucsim/pstr.h
@@ -1,7 +1,7 @@
 /*
  * Simulator of microcontrollers (pstr.h)
  *
- * Copyright (C) 1999,99 Drotos Daniel, Talker Bt.
+ * Copyright (C) 1997,16 Drotos Daniel, Talker Bt.
  * 
  * To contact author send email to drdani@mazsola.iit.uni-miskolc.hu
  *
