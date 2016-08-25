fab12def1b8cb2fa661a7c14b0a26b0564cac151
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2016-08-25 09:01:05 +0000

    Disable some failing long long tests for mcs51.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9750 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-20020508-3.c
+++ sdcc/support/regression/tests/gcc-torture-execute-20020508-3.c
@@ -102,7 +102,7 @@ testTortureExecute (void)
 
   if (ROL (l, SHIFT1) != ROL (LONG_VALUE, SHIFT1))
     ASSERT (0);
-#if !defined (__SDCC_ds390) && !defined (__SDCC_hc08) && !defined (__SDCC_s08)
+#if !defined (__SDCC_mcs51) && !defined (__SDCC_ds390) && !defined (__SDCC_hc08) && !defined (__SDCC_s08)
   if (ROL (ll, shift1) != ROL (LL_VALUE, SHIFT1))
     ASSERT (0);
 
