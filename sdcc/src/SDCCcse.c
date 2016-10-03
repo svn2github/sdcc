3245e11e10fa125e5dc1e0b0bf0bd593c2df0a6b
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2016-10-03 14:05:29 +0000

    Optimize out negation by inverting jump condition, part of RFE #516.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9780 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/SDCCcse.c
+++ sdcc/src/SDCCcse.c
@@ -1663,16 +1663,22 @@ ifxOptimize (iCode * ic, set * cseSet,
           (*change)++;
         }
       else if(ic->prev &&  /* Remove unnecessary casts */
-        (ic->prev->op == '=' || ic->prev->op == CAST) && IS_ITEMP (IC_RESULT (ic->prev)) &&
+        (ic->prev->op == '=' || ic->prev->op == CAST || ic->prev->op == '!') && IS_ITEMP (IC_RESULT (ic->prev)) &&
         IC_RESULT (ic->prev)->key == IC_COND (ic)->key && bitVectnBitsOn (OP_USES (IC_RESULT (ic->prev))) <= 1)
         {
           sym_link *type = operandType (IC_RESULT (ic->prev));
           if (ic->prev->op != CAST || IS_BOOL (type) || bitsForType (operandType (IC_RIGHT (ic->prev))) < bitsForType (type))
           {
-            if (!isOperandVolatile (IC_RIGHT (ic->prev), FALSE))
+            if (!isOperandVolatile (ic->prev->op == '!' ? IC_LEFT (ic->prev) : IC_RIGHT (ic->prev), FALSE))
               {
+                if (ic->prev->op =='!') /* Invert jump logic */
+                  {
+                    symbol *tmp = IC_TRUE (ic);
+                    IC_TRUE (ic) = IC_FALSE (ic);
+                    IC_FALSE (ic) = tmp;
+                  }
                 bitVectUnSetBit (OP_USES (IC_COND (ic)), ic->key);
-                ReplaceOpWithCheaperOp(&IC_COND (ic), IC_RIGHT (ic->prev));
+                ReplaceOpWithCheaperOp(&IC_COND (ic), ic->prev->op == '!' ? IC_LEFT (ic->prev) : IC_RIGHT (ic->prev));
                 (*change)++;
               }
 /* There's an optimization opportunity here, but OP_USES doesn't seem to be */
