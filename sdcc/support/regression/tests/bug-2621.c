9fb1c338add6f2acd4c35d70c5aac23f17118b43
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2017-06-14 21:54:51 +0000

    Disable test for ds390, which doesn't have long long.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9929 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/bug-2621.c
+++ sdcc/support/regression/tests/bug-2621.c
@@ -6,6 +6,8 @@
 
 #include <testfwk.h>
 
+#ifndef __SDCC_ds390
+
 void t(unsigned int i)
 {
 	i;
@@ -63,8 +65,11 @@ void t(unsigned int i)
 	return(v);
 }
 
+#endif
+
 void testBug(void)
 {
+#ifndef __SDCC_ds390
 	ASSERT(f(0x55aa55aa) == 0x55aa55aa);
 	ASSERT(f(0x0000ffff) == 0x0000ffff);
 	ASSERT(f(0xffff0001) == 0xffff0001);
@@ -76,5 +81,6 @@ void testBug(void)
 	ASSERT(h(0x55aa55aa) == 0x55aa55aa);
 	ASSERT(h(0x0000ffff) == 0x0000ffff);
 	ASSERT(h(0xffff0001) == 0xffff00ab);
+#endif
 }
 
