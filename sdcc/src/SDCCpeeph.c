c71105d881e300711773366ca3ba01a21cd31e3a
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2017-08-20 08:49:56 +0000

    Fix bug #2642.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@9995 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/SDCCpeeph.c
+++ sdcc/src/SDCCpeeph.c
@@ -716,6 +716,8 @@ notVolatileVariable(char *var, lineNode *currPl, lineNode *endPl)
         return FALSE;
       if (strchr (var, '[') && strchr (var, ']'))
         return FALSE;
+      if (strstr(var, "0x") || strstr(var, "0X") || isdigit(var[0]))
+        return FALSE;
     }
 
   /* Extract a symbol name from the variable */
