ee97044c5ad9855994163838b42d47880c54404c
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2017-10-28 12:43:17 +0000

    Remove unused infrastructure for port-specific builds.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10119 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/ports/gbz80.mk
+++
@@ -1,9 +0,0 @@
-include components/gbdk-lib.mk
-include components/gbdk-support.mk
-
-gbz80-port: gbz80-lib gbdk-support
-
-gbz80-lib: gbdk-lib-prep
-	PATH=$(PATH):$(BINDIR) $(MAKE) -C $(BUILDDIR)/libc "PLATFORMS=gb rrgb" PORTS=gbz80 SDCCLIB=$(BUILDDIR)
-
-gbz80-port-install: gbz80-port gbdk-support-install
--- sdcc-build/ports/i186.mk
+++
@@ -1,7 +0,0 @@
-# Disable the i186 port
-SDCCCONFIGUREFLAGS += --disable-i186-port
-
-HELPTARGETS += i186-help
-
-i186-help:
-	@echo i186 port is currently disabled
