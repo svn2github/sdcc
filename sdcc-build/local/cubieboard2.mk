71e9573de613e59adb792deb012b5202a440e5f5
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2017-12-01 07:12:22 +0000

    Add special options for build machine cubieboard2.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10187 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc-build/local/cubieboard2.mk
@@ -0,0 +1,18 @@
+# Generate ARMv7 armhf Linux executables on
+# Archlinux on Cubieboard 2
+
+# Spawn 1 make process:
+# spawning 2 processes leads to a compilation error
+MAKEJOBFLAGS = -j 1
+
+HOSTOS = armv7l-unknown-linux-gnueabihf
+TARGETOS = armv7l-unknown-linux-gnueabihf
+ 
+# disable pic14 and pic16 for broken gputils
+SDCCCONFIGUREFLAGS = "--disable-pic14-port --disable-pic16-port"
+
+# local includes and libraries
+TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCPPFLAGS = "-I$(HOME)/local-$(HOSTNAME)/include"
+TARGETLDFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
