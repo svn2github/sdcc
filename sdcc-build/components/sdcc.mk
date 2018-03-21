e8ea2ed95ed5c0fab5ea6cf0ccd6d41a30dad73f
epetrich <epetrich@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2018-03-21 06:29:33 +0000

    * components/sdcc.mk: Only test diagnostics for supported   ports rather than all regression test ports
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10297 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-build/components/sdcc.mk
+++ sdcc-build/components/sdcc.mk
@@ -110,13 +110,13 @@ ifeq ($(CROSSCOMPILING), 1)
 		mkdir -p $(REGTESTDIR); \
 		rm -rf $(_SDCCDIR)/support/regression/gen $(_SDCCDIR)/support/regression/results; \
 		WINEDEBUG=fixme-all $(MAKE) $(MAKESILENTFLAG) -C $(_SDCCDIR)/support/regression SDCC_HOME=$(BUILDDIR) SDCC_EXTRA_DIR=$(SRCDIR)/sdcc-extra $(CROSSREGTESTTARGETS) CROSSCOMPILING=$(CROSSCOMPILING) SDCC="$(WINE) sdcc" WINE=$(WINE) $(CC_FOR_BUILD_STR) 2>&1 | tee $(REGTESTLOG); \
-		WINEDEBUG=fixme-all $(MAKE) $(MAKESILENTFLAG) -C $(_SDCCDIR)/support/valdiag SDCC_HOME=$(BUILDDIR) SDCC_EXTRA_DIR=$(SRCDIR)/sdcc-extra $(CROSSREGTESTTARGETS) CROSSCOMPILING=$(CROSSCOMPILING) SDCC="$(WINE) sdcc" WINE=$(WINE) $(CC_FOR_BUILD_STR) 2>&1 | tee -a $(REGTESTLOG); \
+		WINEDEBUG=fixme-all $(MAKE) $(MAKESILENTFLAG) -C $(_SDCCDIR)/support/valdiag SDCC_HOME=$(BUILDDIR) SDCC_EXTRA_DIR=$(SRCDIR)/sdcc-extra test-ports CROSSCOMPILING=$(CROSSCOMPILING) SDCC="$(WINE) sdcc" WINE=$(WINE) $(CC_FOR_BUILD_STR) 2>&1 | tee -a $(REGTESTLOG); \
 	fi
 else
 	# perform regression tests
 	mkdir -p $(REGTESTDIR); \
 	$(MAKE) $(MAKESILENTFLAG) -C $(_SDCCDIR)/support/regression SDCC_HOME=$(BUILDDIR) SDCC_EXTRA_DIR=$(SRCDIR)/sdcc-extra $(REGTESTTARGETS) 2>&1 | tee $(REGTESTLOG); \
-	$(MAKE) $(MAKESILENTFLAG) -C $(_SDCCDIR)/support/valdiag SDCC_HOME=$(BUILDDIR) SDCC_EXTRA_DIR=$(SRCDIR)/sdcc-extra $(REGTESTTARGETS) 2>&1 | tee -a $(REGTESTLOG)
+	$(MAKE) $(MAKESILENTFLAG) -C $(_SDCCDIR)/support/valdiag SDCC_HOME=$(BUILDDIR) SDCC_EXTRA_DIR=$(SRCDIR)/sdcc-extra test-ports 2>&1 | tee -a $(REGTESTLOG)
 endif
 	echo "--- Regression testing finished on `date` ---"
 
