80f7fca04e576ba0fc3e9b40a208ded7c95436d5
benshi <benshi@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2018-06-08 08:05:31 +0000

    add new makefile config.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10425 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc-build/local/archlinux-respberrypi-3.mk
@@ -0,0 +1,16 @@
+# Generate AARCH64 Linux executables on
+# ArchLinux on Raspberry Pi 3
+# https://archlinuxarm.org/
+
+# Spawn 1 make process:
+# spawning more than 1 processes leads to system freeze
+MAKEJOBFLAGS = -j 1
+
+HOSTOS = aarch64-unknown-linux-gnu
+TARGETOS = arrch64-unknown-linux-gnu
+
+# local includes and libraries
+TARGETCXXFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCFLAGS = "-O2 -I$(HOME)/local-$(HOSTNAME)/include"
+TARGETCPPFLAGS = "-I$(HOME)/local-$(HOSTNAME)/include"
+TARGETLDFLAGS = "-L$(HOME)/local-$(HOSTNAME)/lib"
