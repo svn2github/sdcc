79b80b20fd2ea4c7fa3a2943b8db13660b3b5333
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2018-07-30 08:43:09 +0000

    Add a test from GCC 8.2.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10484 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc/support/regression/tests/gcc-torture-execute-vrp-7.c
@@ -0,0 +1,36 @@
+/*
+vrp-7.c from the execute part of the gcc torture tests.
+*/
+
+#include <testfwk.h>
+
+struct T
+{
+  int b : 1;
+} t;
+
+void foo (int f)
+{
+  t.b = (f & 0x10) ? 1 : 0;
+}
+
+void
+testTortureExecute (void)
+{
+#ifndef __SDCC_z80 // Bug #2786
+#ifndef __SDCC_z180 // Bug #2786
+#ifndef __SDCC_gbz80 // Bug #2786
+#ifndef __SDCC_r2k // Bug #2786
+#ifndef __SDCC_r3ka // Bug #2786
+#ifndef __SDCC_ds390 // Bug #2785
+  foo (0x10);
+  if (!t.b)
+    ASSERT (0);
+  return;
+#endif
+#endif
+#endif
+#endif
+#endif
+#endif
+}
