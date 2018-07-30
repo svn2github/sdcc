f7899b51ad0f9cc502a4cc63b3cb233513e61817
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2018-07-30 08:32:10 +0000

    Disable failing test for hc08
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10483 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-lshrdi-1.c
+++ sdcc/support/regression/tests/gcc-torture-execute-lshrdi-1.c
@@ -208,6 +208,8 @@ constant_shift(unsigned long long x, int i)
 void
 testTortureExecute (void)
 {
+#ifndef __SDCC_hc08 // Bug #2784
+#ifndef __SDCC_s08 // Bug #2784
 #ifndef __SDCC_ds390 // Bug #2783
 #if !(defined(__SDCC_mcs51) && (defined(__SDCC_MODEL_SMALL) || defined(__SDCC_MODEL_MEDIUM) || defined(__SDCC_MODEL_LARGE) && defined(__SDCC_STACK_AUTO))) // Lack of memory
   int i;
@@ -224,6 +226,8 @@ testTortureExecute (void)
       if (y != zext[i])
 	ASSERT (0);
     }
+#endif
+#endif
 #endif
 #endif
   return;
