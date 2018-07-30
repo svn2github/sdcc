8596bb940179471a24fb69a3f524cb68ff269a41
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2018-07-30 09:03:00 +0000

    Add a test from GCC 8.2.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10485 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc/support/regression/tests/gcc-torture-execute-cvt-1.c
@@ -0,0 +1,41 @@
+/*
+cvt-1.c from the execute part of the gcc torture tests.
+*/
+
+#include <testfwk.h>
+
+#ifdef __SDCC
+#pragma disable_warning 93
+#endif
+
+static inline long
+g1 (double x)
+{
+  return (double) (long) x;
+}
+
+long
+g2 (double f)
+{
+  return f;
+}
+
+double
+f (long i)
+{
+  if (g1 (i) != g2 (i))
+    ASSERT (0);
+  return g2 (i);
+}
+
+void
+testTortureExecute (void)
+{
+#if 0 // TODO: Enable when SDCC supports double!
+  if (f (123456789L) != 123456789L)
+    ASSERT (0);
+  if (f (123456789L) != g2 (123456789L))
+    ASSERT (0);
+  return;
+#endif
+}
