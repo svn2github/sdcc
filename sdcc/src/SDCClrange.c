2f61cad91e3e45a5eeb69bf763bc335fcf004c7f
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2018-10-22 12:00:12 +0000

    Fix bug #2826 in preincrement optimization.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10626 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/SDCClrange.c
+++ sdcc/src/SDCClrange.c
@@ -1165,13 +1165,16 @@ shortenLiveRanges (iCode *sic, ebbIndex *ebbi)
     {
       iCode *ifx = 0;
 
-      if (!ic->prev || !ic->next)
-        continue;
-
       iCode *pic = ic->prev;
       iCode *nic = ic->next;
 
+      if (!pic || !nic)
+        continue;
+
       if (ic->op == IFX || nic->op == IFX)
+        continue;
+
+      if (nic->op == IPUSH || nic->op == SEND)
         continue;
 
       if (pic->op != '=' || !IS_ITEMP (IC_RESULT (pic)) || bitVectnBitsOn (OP_DEFS (IC_RESULT (pic))) != 1)
@@ -1193,7 +1196,7 @@ shortenLiveRanges (iCode *sic, ebbIndex *ebbi)
         (POINTER_GET (nic) || isOperandGlobal (IC_LEFT (nic)) || isOperandGlobal (IC_RIGHT (nic))) && (POINTER_SET (ic) || POINTER_SET (nic) && isOperandGlobal (IC_RESULT (ic))))
         continue;
 
-      if (isOperandGlobal (IC_RIGHT (pic))) // Might result in too many globaloperands per op for backend.
+      if (isOperandGlobal (IC_RIGHT (pic))) // Might result in too many global operands per op for backend.
         continue;
 
       if (ifx = ifxForOp (IC_RESULT (nic), nic))
