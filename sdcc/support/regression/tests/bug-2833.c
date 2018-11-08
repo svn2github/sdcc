874845e244f7b3395df26705608d7562d3701d78
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2018-11-08 08:30:00 +0000

    Test for bug #2833.
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10646 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc/support/regression/tests/bug-2833.c
@@ -0,0 +1,30 @@
+/* bug-2833.c
+   A problem resulting in compile-time error on some casts from bool to float.
+ */
+
+#include <testfwk.h>
+
+float cast1 (float a)
+{
+  return !(int)a;
+}
+
+float cast2 (_Bool b)
+{
+  return b;
+}
+
+const float b0 = 0.0f;
+const float b1 = 1.0f;
+
+void testBug(void)
+{
+#if 0
+  ASSERT (cast1 (1.0f) == b0);
+  ASSERT (cast1 (0.0f) == b1);
+
+  ASSERT (cast2 (0.0f) == b0);
+  ASSERT (cast2 (1.0f) == b1);
+#endif
+}
+
