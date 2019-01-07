f8a4f2df6726e796884a4b81f6a8c989c9ebd0d2
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2019-01-07 20:58:09 +0000

    Disable a test for ez80_z80 that is affected by a known bug affecting all z80-related backends
    git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10844 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/gcc-torture-execute-vrp-7.c
+++ sdcc/support/regression/tests/gcc-torture-execute-vrp-7.c
@@ -23,11 +23,13 @@ testTortureExecute (void)
 #ifndef __SDCC_r2k // Bug #2786
 #ifndef __SDCC_r3ka // Bug #2786
 #ifndef __SDCC_tlcs90 // Bug #2786
+#ifndef __SDCC_ez80_z80 // Bug #2786
 #ifndef __SDCC_ds390 // Bug #2785
   foo (0x10);
   if (!t.b)
     ASSERT (0);
   return;
+#endif
 #endif
 #endif
 #endif
